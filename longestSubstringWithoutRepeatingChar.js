c1bf8beb6c28c4904d370d32275f4405eb963812
vokoshyv <ronsuij@gmail.com>
2015-07-24 16:01:25 -0700

    attempted to perform solution, but it was O(n^3) time; whoops

--- longestSubstringWithoutRepeatingChar.js
+++ longestSubstringWithoutRepeatingChar.js
@@ -2,21 +2,33 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-  var ultimateMax = 0;
-  var currentMax = 0;
+
+var hasNoRepeats = function(input){
   var work = {};
-  for (var i = 0; i < s.length; i++){
-    if (work[s[i]] !== undefined){
-      ultimateMax = Math.max(ultimateMax, currentMax);
-      currentMax = i - work[s[i]] + 1;
+  for (var i = 0; i < input.length; i++){
+    if (work[input[i]]){
+      return false;
     }else {
-      work[s[i]] = i;
-      currentMax++;
+      work[input[i]] = true;
     }
   }
-  ultimateMax = Math.max(ultimateMax, currentMax);
-  
-  console.log(ultimateMax);
-  return (ultimateMax !== 0)? ultimateMax : currentMax;
+  return true;
+}
+
+var lengthOfLongestSubstring = function(s) {
+  var work = [];
+  for (var i = 0; i < s.length; i++){
+    for (var j = i+1; j < s.length+1; j++){
+      work.push(s.slice(i, j));
+    }
+  }
+
+  var result = 0;
+
+  work = work.map(function(value){
+    if (hasNoRepeats(value)){
+      result = Math.max(value.length, result);
+    }
+  });
+  return result;
 };
