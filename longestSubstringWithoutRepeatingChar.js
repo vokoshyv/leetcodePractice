7ba06879929c53a6ad3e0935ad05f8ce6870f6b1
vokoshyv <ronsuij@gmail.com>
2015-07-24 15:46:54 -0700

    attempts solution in linear time

--- longestSubstringWithoutRepeatingChar.js
+++ longestSubstringWithoutRepeatingChar.js
@@ -3,5 +3,20 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    
+  var ultimateMax = 0;
+  var currentMax = 0;
+  var work = {};
+  for (var i = 0; i < s.length; i++){
+    if (work[s[i]] !== undefined){
+      ultimateMax = Math.max(ultimateMax, currentMax);
+      currentMax = i - work[s[i]] + 1;
+    }else {
+      work[s[i]] = i;
+      currentMax++;
+    }
+  }
+  ultimateMax = Math.max(ultimateMax, currentMax);
+  
+  console.log(ultimateMax);
+  return (ultimateMax !== 0)? ultimateMax : currentMax;
 };
