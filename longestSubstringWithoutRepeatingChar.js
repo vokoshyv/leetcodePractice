b67da63b2c42841f0d70e6aa5dfce5a6bbc0808d
vokoshyv <ronsuij@gmail.com>
2015-07-24 16:50:01 -0700

    implements solution for longestSubstringWithoutRepeatingChar.js

--- longestSubstringWithoutRepeatingChar.js
+++ longestSubstringWithoutRepeatingChar.js
@@ -7,15 +7,14 @@ var lengthOfLongestSubstring = function(s) {
   var occurred = {};
   var maxLength = 0;
   var currLength = 0;
+  var checkFrom = 0;
 
   for (var i = 0; i < s.length; i++){
-    if (occurred[s[i]] !== undefined){
+    if (occurred[s[i]] !== undefined && occurred[s[i]] >= checkFrom){
       maxLength = Math.max(maxLength, currLength);
-      currLength = i - occurred[s[i]]-1;
-      var temp = i;
-      i = occurred[s[i]] + 1;
-      occurred = {};
-      occurred[s[i]] = temp;
+      currLength = i - occurred[s[i]];
+      checkFrom = occurred[s[i]]; 
+      occurred[s[i]] = i;
     }else {
       currLength++;
       occurred[s[i]] = i;
