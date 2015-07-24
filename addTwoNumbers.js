9287b09e29c47a7c9b324722af20ccbe3f67955b
vokoshyv <ronsuij@gmail.com>
2015-07-24 15:28:09 -0700

    implements solution for addTwoNumbers.js

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -11,57 +11,28 @@
  * @return {ListNode}
  */
 
-var getOnes = function(number){
-  var work = number.toString();
-  return Number(work[work.length-1]);
-}
-
-var ListNode = function(val){
-  this.val = val;
-  this.next = null;
-}
-
 var addTwoNumbers = function(l1, l2) {
-  var work = l1
-  var l1Counter = 0;
-  var l2Counter = 0;
-  
+  var l1String = '';
+  var l2String = '';
+
   while (l1 !== null){
-    l1Counter++;
+    l1String = l1.val.toString() + l1String;
     l1 = l1.next;
   }
   while (l2 !== null){
-    l2Counter++;
+    l2String = l2.val.toString() + l2String;
     l2 = l2.next;
   }
 
-  if (l1Counter > l2Counter){
-    var current = l1;
-    var start = l1;
-  }else{
-    var current = l2;
-    var start = l2;
+  var preWork = (Number(l1String) + Number(l2String)).toString();
+
+  var result = [];
+
+  var i = preWork.length;
+
+  while (i--){
+    result.push(Number(preWork[i]));
   }
 
-  var carry = 0;
-  
-  while (l1.next !== null && l2.next !== null){
-
-    work.val = getOnes(l1.val + l2.val + carry);
-    if ((l1.val + l2.val + carry) > 9){
-      carry = 1;
-    }else {
-      carry = 0;
-    }
-
-    if (l1){
-      l1 = l1.next;
-    }
-    if (l2){
-      l2 = l2.next;
-    }
-    work.next = new ListNode();
-    work = work.next
-  }
-  console.log(origin);
+  return result;
 };
