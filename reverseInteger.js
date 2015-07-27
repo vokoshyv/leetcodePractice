8b8ec6ab7afc61703ce5f62e24718211fa9d3b71
vokoshyv <ronsuij@gmail.com>
2015-07-26 19:26:31 -0700

    implements solution for reverseInteger.js

--- reverseInteger.js
+++ reverseInteger.js
@@ -2,6 +2,17 @@
  * @param {number} x
  * @return {number}
  */
+
+var reverseNumber = function(input){
+  input = input.toString();
+  var work = '';
+  input.split('').map(function(value){
+    work = value + work;
+  })
+  return Number(work);
+}
+
 var reverse = function(x) {
-    
+    var work = (x > 0)? reverseNumber(x) : - reverseNumber(Math.abs(x));
+    return (work > -2147483648 && work < 2147483647)? work : 0;
 };
