f7f2cfb7de1fdc383c5f59db755b714e0c94b78f
vokoshyv <ronsuij@gmail.com>
2015-07-28 15:42:24 -0700

    implements solution for firstMissingPositive.js

--- firstMissingPositive.js
+++ firstMissingPositive.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var firstMissingPositive = function(nums) {
-    
+  var work = [];
+  for (var i = 0; i < nums.length; i++){
+    if (nums[i] > 0){
+      work[nums[i]] = true;
+    }
+  }
+  for (var i = 1; i < work.length; i++){
+    if (work[i] !== true){
+      return i;
+    }
+  }
+  return (work.length > 0)? work.length : 1;
 };
