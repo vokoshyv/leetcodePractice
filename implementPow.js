2fe2f64f314cf1f09b89b4b66116483dd24a4f86
vokoshyv <ronsuij@gmail.com>
2015-07-28 15:54:23 -0700

    starts implementPow.js

--- implementPow.js
+++ implementPow.js
@@ -4,5 +4,23 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    
+    if (n === 0){
+      return 1;
+    }else if (n === 1){
+      return x;
+    }
+    if (n > 0){
+      var work = 1;
+      while (n--){
+        work *= x;
+      }
+      return work;
+    } else {
+      n = Math.abs(n);
+      var work = 1;
+      while (n--){
+        work *= x;
+      }
+      return (1 / work);
+    }
 };
