ae659d7138517e67c22fada1d0866ae2564d6b99
Ron Tsui <vokoshyv@users.noreply.github.com>
2015-07-27 18:06:02 -0700

    adds url for manacher's algorithm on wikipedia

--- longestPalinSubstring.js
+++ longestPalinSubstring.js
@@ -5,6 +5,7 @@
 
 var isPalindrome = function(input){
   // check out manacher's algorithm
+  // https://en.wikipedia.org/wiki/Longest_palindromic_substring
 }
 
 var longestPalindrome = function(s) {
