691b07832cce370de76d410cd11ad586e8952f58
vokoshyv <ronsuij@gmail.com>
2015-07-28 15:09:58 -0700

    implements solution for validParens.js

--- validParens.js
+++ validParens.js
@@ -3,5 +3,21 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-   
+  var stack = [];
+  var lib = {
+    '(': ')', 
+    '{': '}', 
+    '[': ']'
+  };
+
+  for (var i = 0; i < s.length; i++){
+    if ('{[('.indexOf(s[i]) > -1){
+      stack.push(s[i]);
+    }else if (']})'.indexOf(s[i]) > -1){
+      if (lib[stack.pop()] !== s[i]){
+        return false;
+      }
+    }
+  } 
+  return (stack.length === 0)? true : false;
 };
