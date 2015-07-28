e1fd0acc8c3f5ee9efef7ce770757a7845866caa
vokoshyv <ronsuij@gmail.com>
2015-07-28 14:07:10 -0700

    initialzies file for threeSum.js

--- stringToInteger.js
+++ stringToInteger.js
@@ -3,5 +3,5 @@
  * @return {number}
  */
 var myAtoi = function(str) {
-    
+  return Number(str);
 };
---
+++ threeSum.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  var first = {};
+  var second = {};
+  for (var i = 0; i < nums.length; i++){
+    
+  }
+};
