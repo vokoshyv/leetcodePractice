df1e45930de51f7c337ae7cfd59fd64b6105ef89
vokoshyv <ronsuij@gmail.com>
2015-07-29 22:02:19 -0700

    implements solution for lengthLastWord.js

--- lengthLastWord.js
+++ lengthLastWord.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    
+  s = s.split(' ');
+  var work = 0;
+  for (var i = 0; i < s.length; i++){
+    if (s[i].length > 0){
+      work = s[i].length; 
+    }
+  }
+  return work;
 };
