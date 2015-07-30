c511ffbd72b8e280c765fc64b328a335c2d44b26
vokoshyv <ronsuij@gmail.com>
2015-07-29 22:05:04 -0700

    initializes file for lruCache.js

---
+++ lruCache.js
@@ -0,0 +1,23 @@
+/**
+ * @constructor
+ */
+var LRUCache = function(capacity) {
+  this.capacity = capacity;
+};
+
+/**
+ * @param {number} key
+ * @returns {number}
+ */
+LRUCache.prototype.get = function(key) {
+    
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @returns {void}
+ */
+LRUCache.prototype.set = function(key, value) {
+    
+};
