c6ef1cc85e8c16de606c60d3872ce6213079eec7
vokoshyv <ronsuij@gmail.com>
2015-07-30 14:26:27 -0700

    creates faster version of isValid for validParens.js

--- validParens.js
+++ validParens.js
@@ -21,3 +21,27 @@ var isValid = function(s) {
   } 
   return (stack.length === 0)? true : false;
 };
+
+var isValid = function(s){
+  var stack = [];
+  var lib = {
+    '(': ')', 
+    '{': '}', 
+    '[': ']'
+  };
+
+  for (var i = 0; i < s.length; i++){
+    s[i] === '{' && stack.push(s[i]);
+    s[i] === '[' && stack.push(s[i]);
+    s[i] === '(' && stack.push(s[i]);
+    if (s[i] === ')'){
+      if (lib[stack.pop()] !== ')'){return false};
+    }else if (s[i] === '}'){
+      if (lib[stack.pop()] !== '}'){return false};
+    }else if (s[i] === ']'){
+      if (lib[stack.pop()] !== ']'){return false};
+    }
+  }
+
+  return (stack.length === 0)? true : false;
+}
