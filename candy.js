44d6f2449743f8c552b6c2dc4c87bce246380e44
vokoshyv <ronsuij@gmail.com>
2015-08-03 14:26:31 -0700

    implements solution for candy.js

--- candy.js
+++ candy.js
@@ -3,8 +3,33 @@
  * @return {number}
  */
 var candy = function(ratings) {
-  var work = 0;
-  for (var i=0; i < ratings.length; i++){
-    
+  var prev = 1;
+  var latestLocalMaxIndex = 0;
+  var latestLocalMaxValue = 1;
+  var results = 1;
+
+  for (var i = 1; i < ratings.length; i++){
+    if (ratings[i] < ratings[i-1]){
+      // Case 1 : rating is lower than previous value
+      if (i - latestLocalMaxIndex >= latestLocalMaxValue){
+        results += i - latestLocalMaxIndex + 1;
+      }else {
+        results += i - latestLocalMaxIndex;
+      }
+      prev = 1;
+    }else if (ratings[i] > ratings[i-1]){
+      // Case 2 : rating is higher than previous value
+      prev++;
+      results += prev;
+      latestLocalMaxValue = prev;
+      latestLocalMaxIndex = i;
+    }else {
+      // Case 3 : rating is equal to previous value
+      latestLocalMaxIndex = i;
+      latestLocalMaxValue = 1;
+      prev = 1;
+      results += prev;
+    }
   }
+  return results;
 };
