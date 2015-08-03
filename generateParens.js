225100053150ce1a8f98b27b84cedff98682b22a
vokoshyv <ronsuij@gmail.com>
2015-08-03 16:17:37 -0700

    removes console.log to decrease execution time of generateParens.js

--- generateParens.js
+++ generateParens.js
@@ -2,48 +2,11 @@
  * @param {number} n
  * @return {string[]}
  */
-// var generateParenthesis = function(n) {
-//   var work = Array(n+1).join('(').concat(Array(n+1).join(')')).split('');
-//   var results = [];
-
-//   var parenBuild = function(closeAllowed, buildUp, choices){
-//     if (buildUp.length === 2 * n){
-//       console.log(buildUp);
-//       buildUp = buildUp.slice().join('');
-//       results.push(buildUp);
-//       return;
-//     }
-
-//     for (var i = 0; i < choices.length; i++){
-//       var temp = choices.splice(i, 1)[0];
-//       if (temp === '('){
-//         buildUp.push('(');
-//         parenBuild(closeAllowed + 1, buildUp, choices);
-//       } else if (temp === ')' && closeAllowed > 0){
-//         buildUp.push(')');
-//         parenBuild(closeAllowed - 1, buildUp, choices);
-//       }
-//       buildUp.pop();
-//       choices.splice(i, 0, temp);
-//     }
-//   }
-//   parenBuild(0, [], work);
-//   return results;
-// }
-
-// generateParenthesis(2);
-
-/**
- * @param {number} n
- * @return {string[]}
- */
 var generateParenthesis = function(n) {
-  // var work = Array(n+1).join('(').concat(Array(n+1).join(')')).split('');
   var results = [];
 
   var parenBuild = function(closeAllowed, buildUp, openChoice, closeChoice){
     if (buildUp.length === 2 * n){
-      console.log(buildUp);
       buildUp = buildUp.slice().join('');
       results.push(buildUp);
       return;
