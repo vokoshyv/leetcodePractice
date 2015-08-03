6f0fa175ee03eb2fe1174f354e9e504f0323e44b
vokoshyv <ronsuij@gmail.com>
2015-08-03 15:08:22 -0700

    implements solution for waterContainer.js

--- waterContainer.js
+++ waterContainer.js
@@ -3,5 +3,17 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    return 1;
+  var startPointer = 0;
+  var endPointer = height.length-1
+  var maxVolume = 0;
+
+  while (startPointer !== endPointer){
+    maxVolume = Math.max(maxVolume, (endPointer - startPointer) * Math.min(height[startPointer], height[endPointer]));
+    if (height[startPointer] > height[endPointer]){
+      endPointer--;
+    }else {
+      startPointer++;
+    }
+  }
+  return maxVolume;
 };
