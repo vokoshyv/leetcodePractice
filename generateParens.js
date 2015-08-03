89c02f859564afc02e1f7f9bab1287ad99f05d1c
vokoshyv <ronsuij@gmail.com>
2015-08-03 16:13:07 -0700

    implements solution for generateParens.js

--- generateParens.js
+++ generateParens.js
@@ -2,32 +2,66 @@
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function(n) {
-  var work = Array(n+1).join('(').concat(Array(n+1).join(')')).split('');
-  var results = [];
-  console.log(work);
+// var generateParenthesis = function(n) {
+//   var work = Array(n+1).join('(').concat(Array(n+1).join(')')).split('');
+//   var results = [];
 
-  var parenBuild = function(closeAllowed, buildUp, choices){
-    if (choices.length === 0){
-      buildUp = buildUp.join('');
+//   var parenBuild = function(closeAllowed, buildUp, choices){
+//     if (buildUp.length === 2 * n){
+//       console.log(buildUp);
+//       buildUp = buildUp.slice().join('');
+//       results.push(buildUp);
+//       return;
+//     }
+
+//     for (var i = 0; i < choices.length; i++){
+//       var temp = choices.splice(i, 1)[0];
+//       if (temp === '('){
+//         buildUp.push('(');
+//         parenBuild(closeAllowed + 1, buildUp, choices);
+//       } else if (temp === ')' && closeAllowed > 0){
+//         buildUp.push(')');
+//         parenBuild(closeAllowed - 1, buildUp, choices);
+//       }
+//       buildUp.pop();
+//       choices.splice(i, 0, temp);
+//     }
+//   }
+//   parenBuild(0, [], work);
+//   return results;
+// }
+
+// generateParenthesis(2);
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function(n) {
+  // var work = Array(n+1).join('(').concat(Array(n+1).join(')')).split('');
+  var results = [];
+
+  var parenBuild = function(closeAllowed, buildUp, openChoice, closeChoice){
+    if (buildUp.length === 2 * n){
+      console.log(buildUp);
+      buildUp = buildUp.slice().join('');
       results.push(buildUp);
       return;
     }
 
-    for (var i = 0; i < choices.length; i++){
-      var temp = choices.splice(i, 1)[0];
-      if (temp === '('){
-        buildUp.push('(');
-        parenBuild(closeAllowed++, buildUp, choices);
-      } else if (temp === ')' && closeAllowed > 0){
-        buildUp.push(')');
-        parenBuild(closeAllowed--, buildUp, choices);
-      }
+    if (openChoice > 0){
+      buildUp.push('(');
+      parenBuild(closeAllowed + 1, buildUp, openChoice - 1, closeChoice);
       buildUp.pop();
-      choices.splice(i, 0, temp);
+    } 
+    if (closeChoice > 0 && closeAllowed > 0){
+      buildUp.push(')');
+      parenBuild(closeAllowed - 1, buildUp, openChoice, closeChoice - 1);
+      buildUp.pop();
     }
+
   }
-  parenBuild(0, [], work);
+  parenBuild(0, [], n, n);
   return results;
 }
 
