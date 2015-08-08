7d8dfb008a20516e7cea5661a263a108c0a6c4a3
vokoshyv <ronsuij@gmail.com>
2015-08-08 13:52:37 -0700

    creates a recursive function; not totally sure if it works or not

--- islands.js
+++ islands.js
@@ -8,6 +8,32 @@ var numIslands = function(grid) {
   while (i--){
     traveledTo.push(Array.apply(null, Array(grid[0].length)).map(Number.prototype.valueOf,0));
   }
-  console.log(traveledTo);
-  
+
+  var islandCount = 0;
+
+  var traverseTheMap = function(xCoor, yCoor){
+    if (traveledTo[yCoor][xCoor] === 1){
+      return;
+    }
+    traveledTo[yCoor][xCoor] = 1;
+    
+    if (grid[yCoor-1] !== undefined && grid[yCoor-1][xCoor] === 1){
+      traverseTheMap(xCoor, yCoor-1);
+    }
+    if (grid[yCoor+1] !== undefined && grid[yCoor+1][xCoor] === 1){
+      traverseTheMap(xCoor, yCoor+1);
+    }
+    if (grid[yCoor][xCoor-1] !== undefined && grid[yCoor][xCoor-1] === 1){
+      traverseTheMap(xCoor-1, yCoor);
+    }
+    if (grid[yCoor][xCoor+1] !== undefined && grid[yCoor][xCoor+1] === 1){
+      traverseTheMap(xCoor+1, yCoor);
+    }
+
+  }
+
+  for (var yCoor = 0; yCoor < grid.length; yCoor++){
+    for (var xCoor = 0; xCoor < grid[yCoor].)
+  }
+  return islandCount;
 };
