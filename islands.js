6e1e6697b69d0f2335e735268efe681fd62e2e30
vokoshyv <ronsuij@gmail.com>
2015-08-08 12:31:15 -0700

    creates table to keep track of traveled to coordinates

--- islands.js
+++ islands.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    
+  var traveledTo = [];
+  var i = grid.length; 
+  while (i--){
+    traveledTo.push(Array.apply(null, Array(grid[0].length)).map(Number.prototype.valueOf,0));
+  }
+  console.log(traveledTo);
+  
 };
