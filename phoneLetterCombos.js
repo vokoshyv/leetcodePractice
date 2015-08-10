976208c5bfe08b3ab3389cb2d76fd8c333428b63
vokoshyv <ronsuij@gmail.com>
2015-08-10 13:54:23 -0700

    implements solution for phoneLetterCombinations.js

--- phoneLetterCombos.js
+++ phoneLetterCombos.js
@@ -4,4 +4,31 @@
  */
 var letterCombinations = function(digits) {
   
+  var lib = {
+    2: 'abc', 
+    3: 'def', 
+    4: 'ghi', 
+    5: 'jkl', 
+    6: 'mno', 
+    7: 'pqrs', 
+    8: 'tuv', 
+    9: 'wxyz'
+  }
+  var results = [];
+
+  var formWords = function(current){
+    if (current.length === digits.length){
+      results.push(current);
+      return;
+    }
+
+    for (var i = 0; i < lib[digits[current.length]].length; i++){
+      formWords(current + lib[digits[current.length]][i]);
+    }
+  }
+
+  formWords('');
+
+  return (results.length > 1)? results : [];
+
 };
