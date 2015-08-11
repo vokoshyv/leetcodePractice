6c55de53ffff14c64a1fca69d58cfaadcf0ce08b
vokoshyv <ronsuij@gmail.com>
2015-08-10 17:17:49 -0700

    creates matrix to work with

--- spiralMatrix2.js
+++ spiralMatrix2.js
@@ -3,5 +3,10 @@
  * @return {number[][]}
  */
 var generateMatrix = function(n) {
-    
+  var work = [];
+  var i = n;
+  while (i--){
+    work.push(Array.apply(null, Array(n)).map(Number.prototype.valueOf, 0));
+  }
+  console.log(work);
 };
