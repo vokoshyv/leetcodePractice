cb05522569d8b9339c2fecea4a41bb0e2d7ef9a8
vokoshyv <ronsuij@gmail.com>
2015-08-15 12:36:58 -0700

    initializes file for queueUsingStack.js

---
+++ queueUsingStack.js
@@ -0,0 +1,35 @@
+/**
+ * @constructor
+ */
+var Queue = function() {
+    
+};
+
+/**
+ * @param {number} x
+ * @returns {void}
+ */
+Queue.prototype.push = function(x) {
+    
+};
+
+/**
+ * @returns {void}
+ */
+Queue.prototype.pop = function() {
+    
+};
+
+/**
+ * @returns {number}
+ */
+Queue.prototype.peek = function() {
+    
+};
+
+/**
+ * @returns {boolean}
+ */
+Queue.prototype.empty = function() {
+    
+};
