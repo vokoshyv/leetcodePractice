7583e013d5732b391e7db1305af4dff4afcb40e4
vokoshyv <ronsuij@gmail.com>
2015-08-18 09:46:09 -0700

    implements solution for romanToNumeral.js

--- romanToNumeral.js
+++ romanToNumeral.js
@@ -3,5 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+  var lib = {
+    'I': 1, 
+    'V': 5, 
+    'X': 10, 
+    'L': 50,
+    'C': 100,
+    'D': 500, 
+    'M': 1000
+  };
+
+  var total = 0;
+
+  for (var i = 0; i < s.length; i++){
+    if (s[i+1] && lib[s[i]] >= lib[s[i+1]]){
+      total += lib[s[i]];
+    }else if (s[i+1] && lib[s[i]] < lib[s[i+1]]){
+      total -= lib[s[i]];
+    }else {
+      total += lib[s[i]];
+    }
+  }
+
+  return total;
 };
