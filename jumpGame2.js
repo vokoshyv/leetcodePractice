67fb1eb3bf141d588b9f78d36404947fb186fd58
vokoshyv <ronsuij@gmail.com>
2015-08-17 19:48:24 -0700

    not quite there for jumpGame2.js yet

--- jumpGame2.js
+++ jumpGame2.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
 var jump = function(nums) {
-    
+  var tries = [];
+
+  var jumpAlong = function(currIndex, jumps){
+    if (currIndex >= nums.length-1){
+      tries.push(jumps);
+      return;
+    }
+
+    for (var i = nums[currIndex] - 1; i > 0; i--){
+      jumpAlong(currIndex + i, jumps + 1);
+    }
+  }
+  jumpAlong(0, 0);
+  console.log(tries);
+  return Math.min.apply(this, tries);
 };
