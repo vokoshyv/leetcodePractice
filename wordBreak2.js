1a33ac9d06d11bc3e2107bfe9e001dc30a334e33
vokoshyv <ronsuij@gmail.com>
2015-08-18 15:16:32 -0700

    almost there on wordBreak2.js

--- wordBreak2.js
+++ wordBreak2.js
@@ -4,5 +4,24 @@
  * @return {string[]}
  */
 var wordBreak = function(s, wordDict) {
-    
+  var results = [];
+
+  var findCombos = function(current, pullFrom){
+    if (pullFrom.length === 0){
+      results.push(current.join(' '));
+      return;
+    }
+
+    for (var i = 0; i < wordDict.length; i++){
+      if (pullFrom.indexOf(wordDict[i]) === 0){
+        current.push(wordDict[i]);
+        findCombos(current, pullFrom.slice(wordDict[i].length));
+        current.pop();
+      }
+    }
+  }
+  findCombos([], s);
+  return results;
 };
+
+wordBreak('catsanddog', ["cat", "cats", "and", "sand", "dog"]);
