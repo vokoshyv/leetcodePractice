21d1fa234124bd11182cf3815c933c35b3ba9afb
vokoshyv <ronsuij@gmail.com>
2015-08-18 12:12:11 -0700

    implements solution for reverseWords.js

--- reverseWords.js
+++ reverseWords.js
@@ -3,5 +3,20 @@
  * @returns {string}
  */
 var reverseWords = function(str) {
-    
+  str = str.split(' ');
+
+  str = str.filter(function(value){return value.length > 0});
+  
+  var start = 0;
+  var end = str.length-1;
+  var temp;
+
+  while (start < end){
+    temp = str[start];
+    str[start] = str[end];
+    str[end] = temp;
+    start++;
+    end--;
+  }
+  return str.join(' ');
 };
