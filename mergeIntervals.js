e0166ee5f8319529ce736070e60857b216951e2c
vokoshyv <ronsuij@gmail.com>
2015-08-19 12:03:56 -0700

    initializes file for mergeIntervals.js

---
+++ mergeIntervals.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @return {Interval[]}
+ */
+var merge = function(intervals) {
+    
+};
--- singleNumber.js
+++ singleNumber.js
@@ -2,6 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+function dec2bin(dec){
+    return (dec >>> 0).toString(2);
+}
+
+
 var singleNumber = function(nums) {
-    
+  var num = 0;
+  for (var i = 0; i < nums.length; i++){
+    num ^= nums[i];
+  }
+  return num;
 };
