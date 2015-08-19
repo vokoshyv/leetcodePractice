3b55fa4ff6e6484e04d2ebd7d3e8890f3d921f79
vokoshyv <ronsuij@gmail.com>
2015-08-19 11:00:22 -0700

    will try this one again sometime: reorderList.js

--- reorderList.js
+++ reorderList.js
@@ -10,5 +10,26 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function(head) {
-    
+  console.log(head);
+  var last;
+  if (head !== null && head.next !== undefined && head.next !== null){
+    var toConnect = head.next;
+    if (head.next.next === null){
+      return head;
+    }
+  }
+  var work = head;
+  
+  while (work !== null){
+    if (work.next === null){
+      last = work;
+    }
+    work = work.next;
+  }
+
+  if (head !== null && head.next !== undefined && head.next !== null){
+    head.next = last;
+    last.next = toConnect;
+  }
+  return head;
 };
