9d3ad4dc3a2fbed2245e61123fc8b0636ebbbced
vokoshyv <ronsuij@gmail.com>
2015-08-19 10:47:52 -0700

    initializes file for reorderList.js

---
+++ reorderList.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) {
+    
+};
