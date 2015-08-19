68c7858adfa121747f65d46618b501c61dce3267
vokoshyv <ronsuij@gmail.com>
2015-08-19 10:42:25 -0700

    implements solution for powerOfTwo.js

--- powerOfTwo.js
+++ powerOfTwo.js
@@ -3,5 +3,14 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    
+  if (n < 1){return false};
+
+  var check = 1;
+  while (check <= n){
+    if (check === n){
+      return true;
+    }
+    check *= 2;
+  }
+  return false;
 };
