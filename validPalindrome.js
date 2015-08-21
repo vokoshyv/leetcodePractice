f0d9f8f4179c45834b5c2559768e99f7389b2c7b
vokoshyv <ronsuij@gmail.com>
2015-08-21 10:04:10 -0700

    implements solution for validPalindrome.js

--- validPalindrome.js
+++ validPalindrome.js
@@ -3,5 +3,23 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    
+  var lib = '1234567890abcdefghijklmnopqrstuvwxyz'
+  var libObject = {};
+  for (var i = 0; i < lib.length; i++){
+    libObject[lib[i]] = true;
+  }
+
+  var work = '';
+  for (var i = 0; i < s.length; i++){
+    if (libObject[s[i].toLowerCase()]){
+      work += s[i].toLowerCase();
+    }
+  }
+
+  var index = work.length;
+  var workCompare = '';
+  while (index--){
+    workCompare += work[index];
+  }
+  return work === workCompare;
 };
