38e549c39074c5a7c0a7fc6f4e682b985ea63975
vokoshyv <ronsuij@gmail.com>
2015-08-21 10:16:39 -0700

    implements solution, but it works in linear time complexity with linear space complexity; have yet to implement constant space complexity

--- singleNumber3.js
+++ singleNumber3.js
@@ -3,5 +3,25 @@
  * @return {number[]}
  */
 var singleNumber = function(nums) {
-    
+  // var index = nums.length; 
+
+  // var check1 = 0;
+  // var check2 = 0;
+
+  // nums.forEach(function(value){check1 ^= value});
+  // while (index--){check2 ^= nums[index]};
+
+  // console.log(check1);
+  // console.log(check2);
+
+  var work = {};
+  nums.forEach(function(value){
+    if (work[value] === undefined){
+      work[value] = true;
+    } else {
+      delete work[value];
+    }
+  })
+
+  return Object.keys(work).map(function(value){return Number(value)});
 };
