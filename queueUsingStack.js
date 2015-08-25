6d4b54b5e9550d94e9a6af08cc2a32ac4bd605ac
vokoshyv <ronsuij@gmail.com>
2015-08-25 15:34:31 -0700

    implements solution for queueUsingStack.js

--- queueUsingStack.js
+++ queueUsingStack.js
@@ -2,7 +2,8 @@
  * @constructor
  */
 var Queue = function() {
-    
+  this.storage1 = [];
+  this.storage2 = [];
 };
 
 /**
@@ -10,26 +11,35 @@ var Queue = function() {
  * @returns {void}
  */
 Queue.prototype.push = function(x) {
-    
+  this.storage1.push(x);
 };
 
 /**
  * @returns {void}
  */
 Queue.prototype.pop = function() {
-    
+  while (this.storage1.length > 0){
+    var temp = this.storage1.pop();
+    this.storage2.push(temp);
+  }
+  var result = this.storage2.pop();
+  while (this.storage2.length > 0){
+    temp = this.storage2.pop();
+    this.storage1.push(temp);
+  }
+  return result;
 };
 
 /**
  * @returns {number}
  */
 Queue.prototype.peek = function() {
-    
+  return this.storage1[0];
 };
 
 /**
  * @returns {boolean}
  */
 Queue.prototype.empty = function() {
-    
+  return this.storage1.length === 0;
 };
