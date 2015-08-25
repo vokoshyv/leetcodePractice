bd2e1f139847aad958e269a69ed257366b3781da
vokoshyv <ronsuij@gmail.com>
2015-08-25 15:42:50 -0700

    implements solution for findPeakElement.js

--- findPeakElement.js
+++ findPeakElement.js
@@ -3,5 +3,16 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    
+  if (nums.length === 1){
+    return 0;
+  }
+  for (var i = 0; i < nums.length; i++){
+    if (i === 0 && nums[i] > nums[i+1]){
+      return i;
+    } else if (i === nums.length-1 && nums[i] > nums[i-1]){
+      return i;
+    } else if (nums[i] > nums[i-1] && nums[i] > nums[i+1]){
+      return i;
+    }
+  }
 };
