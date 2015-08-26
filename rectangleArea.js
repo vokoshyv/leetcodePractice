cb144c777edad0029161b74232df90b9f5ac46d7
vokoshyv <ronsuij@gmail.com>
2015-08-26 16:36:20 -0700

    implements solution for rectangleArea.js

--- rectangleArea.js
+++ rectangleArea.js
@@ -10,5 +10,8 @@
  * @return {number}
  */
 var computeArea = function(A, B, C, D, E, F, G, H) {
-    
+  var firstRecArea = (C-A)*(D-B);
+  var secondRecArea = (G-E)*(H-F);
+  var overlap = Math.max(0, (Math.min(C, G) - Math.max(A, E))) * Math.max(0, (Math.min(D, H) - Math.max(B, F)));
+  return firstRecArea + secondRecArea - overlap;
 };
