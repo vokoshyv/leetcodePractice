ada6daacac69590b77c4ac0f62ad64cd523efd3d
vokoshyv <ronsuij@gmail.com>
2015-08-26 16:21:37 -0700

    initializes file for rectangleArea.js

---
+++ rectangleArea.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} A
+ * @param {number} B
+ * @param {number} C
+ * @param {number} D
+ * @param {number} E
+ * @param {number} F
+ * @param {number} G
+ * @param {number} H
+ * @return {number}
+ */
+var computeArea = function(A, B, C, D, E, F, G, H) {
+    
+};
