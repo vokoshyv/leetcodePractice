6481fa1e203cb6db1627db65855d58bbe751a24d
vokoshyv <ronsuij@gmail.com>
2015-09-01 17:27:02 -0700

    implements solution for majorityElement2.js

--- majorityElement2.js
+++ majorityElement2.js
@@ -3,5 +3,24 @@
  * @return {number[]}
  */
 var majorityElement = function(nums) {
-    
+  var work = {};
+
+  var results = [];
+
+  for (var i = 0; i < nums.length; i++){
+    if (work[nums[i]] !== undefined){
+      work[nums[i]]++;
+    } else {
+      work[nums[i]] = 1;
+    }
+  }
+
+  var checkAgainst = Math.floor(nums.length/3);
+
+  for (var key in work){
+    if (work[key] > checkAgainst){
+      results.push(Number(key));
+    }
+  }
+  return results;
 };
