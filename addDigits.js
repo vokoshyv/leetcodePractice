0027ed0336807e6e7e46c9d974d69b554261c451
vokoshyv <ronsuij@gmail.com>
2015-09-03 16:46:05 -0700

    initializes file for addDigits.js

---
+++ addDigits.js
@@ -0,0 +1,16 @@
+/* 
+* @Author: humanityloaner
+* @Date:   2015-09-03 16:45:24
+* @Last Modified by:   humanityloaner
+* @Last Modified time: 2015-09-03 16:45:51
+*/
+
+'use strict';
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var addDigits = function(num) {
+    
+};
