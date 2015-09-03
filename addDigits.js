efa804bf297ef49b09a78fcacc24803232486fa8
vokoshyv <ronsuij@gmail.com>
2015-09-03 16:58:32 -0700

    initializes file for largestNumber.js

--- addDigits.js
+++ addDigits.js
@@ -2,7 +2,7 @@
 * @Author: humanityloaner
 * @Date:   2015-09-03 16:45:24
 * @Last Modified by:   humanityloaner
-* @Last Modified time: 2015-09-03 16:48:18
+* @Last Modified time: 2015-09-03 16:52:49
 */
 
 'use strict';
@@ -12,8 +12,5 @@
  * @return {number}
  */
 var addDigits = function(num) {
-  while (num.toString().length > 1){
-    num = num.toString().split('').reduce(function(prev, curr){return Number(prev) + Number(curr)});
-  }
-  return num;
+  return (num - 9) * Math.floor((num-1)/9);
 };
---
+++ largestNumber.js
@@ -0,0 +1,16 @@
+/* 
+* @Author: humanityloaner
+* @Date:   2015-09-03 16:58:11
+* @Last Modified by:   humanityloaner
+* @Last Modified time: 2015-09-03 16:58:23
+*/
+
+'use strict';
+
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    
+};
