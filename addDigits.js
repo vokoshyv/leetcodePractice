7557d214c7da6c719f9a247ffaecd44d35af5efb
vokoshyv <ronsuij@gmail.com>
2015-09-03 16:50:33 -0700

    impmlements solution for addDigits.js

--- addDigits.js
+++ addDigits.js
@@ -2,7 +2,7 @@
 * @Author: humanityloaner
 * @Date:   2015-09-03 16:45:24
 * @Last Modified by:   humanityloaner
-* @Last Modified time: 2015-09-03 16:45:51
+* @Last Modified time: 2015-09-03 16:48:18
 */
 
 'use strict';
@@ -12,5 +12,8 @@
  * @return {number}
  */
 var addDigits = function(num) {
-    
+  while (num.toString().length > 1){
+    num = num.toString().split('').reduce(function(prev, curr){return Number(prev) + Number(curr)});
+  }
+  return num;
 };
