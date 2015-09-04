d3d1ecd4fce63442101d7d3d60fa2e0d0e07e4b5
vokoshyv <ronsuij@gmail.com>
2015-09-03 17:50:22 -0700

    implements slightly faster solution for productOfArrayExceptSelf.js

--- productOfArrayExceptSelf.js
+++ productOfArrayExceptSelf.js
@@ -2,7 +2,7 @@
 * @Author: humanityloaner
 * @Date:   2015-09-03 17:08:13
 * @Last Modified by:   humanityloaner
-* @Last Modified time: 2015-09-03 17:33:23
+* @Last Modified time: 2015-09-03 17:49:18
 */
 
 'use strict';
@@ -13,22 +13,22 @@
  */
 var productExceptSelf = function(arr) {
   var work = [];
-  var check = 1;
-  for (var i = 1; i < arr.length; i++){
-  	if (work[i] === undefined){
-  		work[i] = 1;
-  	}
-  	work[i] *= (arr[i-1] * check);
-  	check *= arr[i-1];
-  }
-  check = 1;
+  var checkFor = 1;
+  var checkBack = 1;
+  var size = arr.length;
 
-  for (var i = arr.length-2; i > -1; i--){
-  	if (i === 0){
-  		work[i] = 1;
-  	}
-  	work[i] *= (arr[i+1] * check);
-  	check *= arr[i+1];
+  for (var i = 1; i < arr.length; i++){
+    if (work[i] === undefined){
+      work[i] = 1;
+    };
+    if (work[size-i-1] === undefined){
+      work[size-i-1] = 1;
+    }
+    work[i] *= (arr[i-1] * checkFor);
+    work[size-i-1] *= (arr[size-i] * checkBack);
+    checkFor *= arr[i-1];
+    checkBack *= arr[size-i];
   }
+
   return work;
 };
