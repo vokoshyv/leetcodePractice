1023337944193e73cd11004a9bd55c5f7d8698d7
vokoshyv <ronsuij@gmail.com>
2015-09-03 18:53:15 -0700

    initializes file for uniquePaths.js

---
+++ uniquePaths.js
@@ -0,0 +1,17 @@
+/* 
+* @Author: humanityloaner
+* @Date:   2015-09-03 18:52:48
+* @Last Modified by:   humanityloaner
+* @Last Modified time: 2015-09-03 18:53:04
+*/
+
+'use strict';
+
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function(m, n) {
+    
+};
