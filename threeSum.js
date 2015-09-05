4a3d71d524a04a79a4af551a7dc7b78975ec73f0
vokoshyv <ronsuij@gmail.com>
2015-09-04 19:41:47 -0700

    tried some more stuff; not quite there yet

--- threeSum.js
+++ threeSum.js
@@ -3,5 +3,27 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    
+  var check = {};
+  var otherCheck = [];
+  var results = [];
+  var work;
+
+  nums.forEach(function(value){
+    work = [value];
+    for (var i = 0; i < otherCheck.length; i++){
+      if (check[- otherCheck[i] - value] !== undefined){
+        work.push(otherCheck[i]);
+        work.push(check[-otherChek[i] - value]);
+        results.push(work.slice());
+        work.pop().pop();
+      } else {
+        check[value] = true;
+        otherCheck.push(value);
+      }
+    }
+  })
+  return results;
 };
+
+
+threeSum([-1, 0, 1, 2, -1, -4]);
