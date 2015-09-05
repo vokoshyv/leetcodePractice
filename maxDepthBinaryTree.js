21a21f6b3fc20b0789b0e1ef977bc14d6ce9cdc8
vokoshyv <ronsuij@gmail.com>
2015-09-05 16:17:25 -0700

    implements solution for maxDepthBinaryTree.js

--- maxDepthBinaryTree.js
+++ maxDepthBinaryTree.js
@@ -11,14 +11,17 @@
  */
 
 var maxDepth = function(root) {
-	var result = 1;
+	var result = 0;
 
 	var searchDeep = function(node, depth){
-
+		if (node === null){
+			result = Math.max(result, depth);
+			return;
+		}
 
 		searchDeep(node.right, depth + 1);
 		searchDeep(node.left, depth + 1);
 	}
-	searchDeep(root, 1);
+	searchDeep(root, 0);
 	return result;
 };
