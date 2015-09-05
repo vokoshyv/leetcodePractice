d528fc7ceb4a7665a4abbee6ed4c6de36e46b35c
vokoshyv <ronsuij@gmail.com>
2015-09-05 16:53:17 -0700

    initializes file for findMinimumRotated.js

---
+++ findMinimumRotated.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+	// return Math.min.apply(this, nums);
+	var start = 0;
+	var end = nums.length -1; 
+	var mid = Math.floor((end + start) / 2);
+
+
+};
