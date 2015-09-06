2f88adb4ac0d94d523ac6b594e80c199d604df15
vokoshyv <ronsuij@gmail.com>
2015-09-05 17:08:21 -0700

    willl return to findMinimumRotated.js

--- findMinimumRotated.js
+++ findMinimumRotated.js
@@ -5,8 +5,11 @@
 var findMin = function(nums) {
 	// return Math.min.apply(this, nums);
 	var start = 0;
-	var end = nums.length -1; 
+	var end = nums.length -2; 
 	var mid = Math.floor((end + start) / 2);
 
-
+	while (start !== end){
+		
+	}
+	return end;
 };
