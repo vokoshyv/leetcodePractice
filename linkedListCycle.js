e55ea4bed3f9e9921296622a7b13acb3422cd65b
vokoshyv <ronsuij@gmail.com>
2015-09-05 17:31:39 -0700

    adds a third method to come

--- linkedListCycle.js
+++ linkedListCycle.js
@@ -11,17 +11,52 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    console.log(head);
-    var counter = 0;
-    while (head !== null){
-        if (head === null){
-            return false;
-        }
-        head = head.next;
-        counter++;
-        if (counter > 1000000){
-            return true;
-        }
-    }
-    return false;
+    // 1) METHOD ONE
+    // console.log(head);
+    // var counter = 0;
+    // while (head !== null){
+    //     if (head === null){
+    //         return false;
+    //     }
+    //     head = head.next;
+    //     counter++;
+    //     if (counter > 8030){
+    //         return true;
+    //     }
+    // }
+    // return false;
+
+    // 2) METHOD TWO
+    // var turtle = head;
+    // if (head !== null && head.next !== undefined){
+    // 	var hare = head.next;
+    // } else {
+    // 	return false;
+    // }
+    // var check = true;
+
+    // while (turtle !== null && hare !== null){
+    // 	if (turtle === hare){
+    // 		return true;
+    // 	}
+    // 	if (check){
+    // 		turtle = turtle.next;
+    // 		hare = hare.next;
+    // 		check = false;
+    // 	} else {
+    // 		hare = hare.next;
+    // 		check = true;
+    // 	}
+    // }
+
+    // return false;
+
+    // 3) METHOD 3
+	
+	
+
+
 };
+
+
+
