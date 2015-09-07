9d48c3f653d7f3d1f9fbb24b532c5b8e61170371
vokoshyv <ronsuij@gmail.com>
2015-09-07 16:32:32 -0700

    attempted solution, but the string length got to be invalid

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -3,5 +3,30 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
-    
+  // var work = [];
+  // for (var i = 0; i < nums.length; i++){
+  //   work[nums[i]] = 'blah';
+  // }
+
+  // work = work.join('OOO');
+  // work += 'OOO';
+  // work = 'OOO' + work;
+
+  // work = work.split('blah');
+
+  // var max = 0;
+  // var current = 0;
+
+  // for (var i = 0; i < work.length; i++){
+  //   if (work[i] !== 'OOO'){
+  //     max = Math.max(max, current);
+  //     current = 0;
+  //   } else {
+  //     current++;
+  //     max = Math.max(max, current);
+  //   }
+  // }
+  // return max;
+
+  
 };
