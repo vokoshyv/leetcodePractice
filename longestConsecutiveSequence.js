7443130a4f9aa11aa65c762bd73c3b4b19748518
vokoshyv <ronsuij@gmail.com>
2015-09-07 17:12:29 -0700

    implements much faster solution to longestConsecutiveSequence.js

--- longestConsecutiveSequence.js
+++ longestConsecutiveSequence.js
@@ -1,211 +1,27 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-
-var HashTable = function() {
-  this._storage = [];
-  this._count = 0;
-  this._limit = 8;
-}
-
-
-HashTable.prototype.insert = function(key, value) {
-  //create an index for our storage location by passing it through our hashing function
-  var index = this.hashFunc(key, this._limit);
-  //retrieve the bucket at this particular index in our storage, if one exists
-  //[[ [k,v], [k,v], [k,v] ] , [ [k,v], [k,v] ]  [ [k,v] ] ]
-  var bucket = this._storage[index]
-    //does a bucket exist or do we get undefined when trying to retrieve said index?
-  if (!bucket) {
-    //create the bucket
-    var bucket = [];
-    //insert the bucket into our hashTable
-    this._storage[index] = bucket;
-  }
-
-  var override = false;
-  //now iterate through our bucket to see if there are any conflicting
-  //key value pairs within our bucket. If there are any, override them.
-  for (var i = 0; i < bucket.length; i++) {
-    var tuple = bucket[i];
-    if (tuple[0] === key) {
-      //overide value stored at this key
-      tuple[1] = value;
-      override = true;
-    }
-  }
-
-  if (!override) {
-    //create a new tuple in our bucket
-    //note that this could either be the new empty bucket we created above
-    //or a bucket with other tupules with keys that are different than 
-    //the key of the tuple we are inserting. These tupules are in the same
-    //bucket because their keys all equate to the same numeric index when
-    //passing through our hash function.
-    bucket.push([key, value]);
-    this._count++
-      //now that we've added our new key/val pair to our storage
-      //let's check to see if we need to resize our storage
-      if (this._count > this._limit * 0.75) {
-        this.resize(this._limit * 2);
-      }
-  }
-  return this;
-};
-
-
-HashTable.prototype.remove = function(key) {
-  var index = this.hashFunc(key, this._limit);
-  var bucket = this._storage[index];
-  if (!bucket) {
-    return null;
-  }
-  //iterate over the bucket
-  for (var i = 0; i < bucket.length; i++) {
-    var tuple = bucket[i];
-    //check to see if key is inside bucket
-    if (tuple[0] === key) {
-      //if it is, get rid of this tuple
-      bucket.splice(i, 1);
-      this._count--;
-      if (this._count < this._limit * 0.25) {
-        this.resize(this._limit / 2);
-      }
-      return tuple[1];
-    }
-  }
-};
-
-
-
-HashTable.prototype.retrieve = function(key) {
-  var index = this.hashFunc(key, this._limit);
-  var bucket = this._storage[index];
-
-  if (!bucket) {
-    return null;
-  }
-
-  for (var i = 0; i < bucket.length; i++) {
-    var tuple = bucket[i];
-    if (tuple[0] === key) {
-      return tuple[1];
-    }
-  }
-
-  return null;
-};
-
-
-HashTable.prototype.hashFunc = function(str, max) {
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
-    var letter = str[i];
-    hash = (hash << 5) + letter.charCodeAt(0);
-    hash = (hash & hash) % max;
-  }
-  return hash;
-};
-
-
-HashTable.prototype.resize = function(newLimit) {
-  var oldStorage = this._storage;
-
-  this._limit = newLimit;
-  this._count = 0;
-  this._storage = [];
-
-  oldStorage.forEach(function(bucket) {
-    if (!bucket) {
-      return;
-    }
-    for (var i = 0; i < bucket.length; i++) {
-      var tuple = bucket[i];
-      this.insert(tuple[0], tuple[1]);
-    }
-  }.bind(this));
-};
-
-
-HashTable.prototype.retrieveAll = function() {
-  console.log(this._storage);
-  //console.log(this._limit);
-};
-
-
-
 var longestConsecutive = function(nums) {
-  // var work = [];
-  // for (var i = 0; i < nums.length; i++){
-  //   work[nums[i]] = 'blah';
-  // }
 
-  // // work = work.join('OOO');
-  // // work += 'OOO';
-  // // work = 'OOO' + work;
-
-  // // work = work.split('blah');
-
-  // // var max = 0;
-  // // var current = 0;
-
-  // // for (var i = 0; i < work.length; i++){
-  // //   if (work[i] !== 'OOO'){
-  // //     max = Math.max(max, current);
-  // //     current = 0;
-  // //   } else {
-  // //     current++;
-  // //     max = Math.max(max, current);
-  // //   }
-  // // }
-  // // return max;
-
-  // var listOfLocations = [];
-
-  // for (var index in work){
-  //   listOfLocations.push(Number(index));
-  // }
-
-  // console.log(listOfLocations);
-
-  // var max = 1; 
-  // var current = 1;
-  // for (var i = 1; i < listOfLocations.length; i++){
-  //   if (listOfLocations[i] !== listOfLocations[i-1] + 1){
-  //     max = Math.max(current, max);
-  //     current = 1;
-  //   } else {
-  //     current++;
-  //     max = Math.max(current, max);
-  //   }
-  // }
-  // return max;
-
-  var work = new HashTable();
+  var work = {};
 
   for (var i = 0; i < nums.length; i++){
-    work.insert(nums[i], true);
+    work[nums[i]] = true;
   }
-
   var max = 1;
   var left;
   var right;
   var count;
 
-  for (var i = 0; i < nums.length; i++){
+  for (i = 0; i < nums.length; i++){
     left = nums[i] - 1;
     right = nums[i] + 1;
     count = 1;
 
-    while (work.retrieve(left) !== null){
-      work.remove(left);
+    while (work[left] !== undefined){
+      delete work[left];
       left--;
       count++;
     }
-
-    while (work.retrieve(right) !== null){
-      work.remove(right);
+    while (work[right] !== undefined){
+      delete work[right];
       right++;
       count++;
     }
