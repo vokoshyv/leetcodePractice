02f29f48927ee97b1a4bad0ccfe088848bd72fff
vokoshyv <ronsuij@gmail.com>
2015-09-08 18:18:04 -0700

    initializes file for binaryTreePaths.js

---
+++ binaryTreePaths.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    
+};
