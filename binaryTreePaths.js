9194ed05afa56ee46eda07cc0ba3b4d977edd09d
vokoshyv <ronsuij@gmail.com>
2015-09-12 15:14:22 -0700

    implements solution but it is quite slow

--- binaryTreePaths.js
+++ binaryTreePaths.js
@@ -10,8 +10,34 @@
  * @return {string[]}
  */
 var binaryTreePaths = function(root) {
-  var results = [];
+  console.log(JSON.stringify(root));
+  var preResults = [];
 
+  var findPaths = function(prevNode, buildUp){
+
+    if (prevNode === null){
+      return;
+    } else if (prevNode.right === null && prevNode.left === null){
+      buildUp.push(prevNode.val);
+      preResults.push(buildUp.slice());
+      buildUp.pop();
+      return;
+    }
+
+    buildUp.push(prevNode.val);
+    findPaths(prevNode.right, buildUp);
+    findPaths(prevNode.left, buildUp);
+    buildUp.pop();
+  }
+  findPaths(root, []);
+
+  console.log(preResults);
   
+  var results = [];
+  for (var i = 0; i < preResults.length; i++){
+    if (preResults[i].length > 0){
+      results.push(preResults[i].join('->'));
+    }
+  }
   return results;
 };
