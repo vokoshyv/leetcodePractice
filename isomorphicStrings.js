7b0da745c743a66773a826a8bc6921cc160230ee
Ron Tsui <ronsuij@gmail.com>
2015-09-13 18:09:05 -0700

    implements solution for isomorphicStrings.js

--- isomorphicStrings.js
+++ isomorphicStrings.js
@@ -4,5 +4,33 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    
+  if (s.length !== t.length){
+    return false;
+  }
+  
+  var lib = {};
+
+  for (var i = 0; i < s.length; i++){
+    if (lib[s[i]] !== undefined){
+      if (lib[s[i]] !== t[i]){
+        return false;
+      }
+    } else {
+      lib[s[i]] = t[i];
+    }
+  }
+
+  var lib2 = {};
+
+  for (var i = 0; i < t.length; i++){
+    if (lib2[t[i]] !== undefined){
+      if (lib2[t[i]] !== s[i]){
+        return false;
+      }
+    } else {
+      lib2[t[i]] = s[i];
+    }
+  }
+
+  return true;
 };
