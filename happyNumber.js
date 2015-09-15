1e8373f759d9f27d04d4e3798cee95ba81614942
vokoshyv <ronsuij@gmail.com>
2015-09-14 20:26:17 -0700

    will return to happyNumbers.js

--- happyNumber.js
+++ happyNumber.js
@@ -3,5 +3,27 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-    
+  var temp;
+  var lib = {};
+  var count = 1;
+
+  while (n !== 1){
+    // if (lib[n] !== undefined){
+    //   return false;
+    // }
+
+    if (count > 10000){
+      console.log(false);
+      return false;
+    }
+    n = (n).toString().split('').reduce(function(prev, curr){
+      return Number(prev) * Number(prev) + Number(curr) * Number(curr);
+    })
+    count++;
+  }
+
+  console.log(true);
+  return true;
 };
+
+isHappy(13);
