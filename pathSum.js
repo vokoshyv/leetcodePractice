e3466b2903772e498f1aa3508667e76cbbecc78e
vokoshyv <ronsuij@gmail.com>
2015-09-14 20:55:37 -0700

    implements solution for pathSum.js

--- pathSum.js
+++ pathSum.js
@@ -11,19 +11,15 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, sum) {
-  var result = false;
 
   if (root === null){
     return false;
   }
 
+  var result = false;
+
   var search = function(currentSum, currentNode){
-    if (currentNode === null){
-      if (currentSum === sum){
-        result = true;
-      }
-      return;
-    } else if (currentNode.right === null && currentNode.left === null){
+    if (currentNode !== null && currentNode.right === null && currentNode.left === null){
       currentSum += currentNode.val;
       if (currentSum === sum){
         result = true;
@@ -32,8 +28,10 @@ var hasPathSum = function(root, sum) {
       return;
     }
 
-    search(currentSum + currentNode.val, currentNode.right);
-    search(currentSum + currentNode.val, currentNode.left);
+    if (currentNode !== null){
+      search(currentSum + currentNode.val, currentNode.right);
+      search(currentSum + currentNode.val, currentNode.left);
+    }
 
   }
   search(0, root);
