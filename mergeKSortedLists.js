a7f8af05069dcdbfc4fa1056d92606516805e423
vokoshyv <ronsuij@gmail.com>
2015-09-15 13:58:27 -0700

    initializes file for mergeKSortedLists.js

---
+++ mergeKSortedLists.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function(lists) {
+    
+};
