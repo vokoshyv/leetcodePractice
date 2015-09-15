d1b4b767cf79f520cbb5642277f3b061a38cec2a
vokoshyv <ronsuij@gmail.com>
2015-09-15 14:16:54 -0700

    implements solution for mergeKSortedLists.js

--- mergeKSortedLists.js
+++ mergeKSortedLists.js
@@ -9,6 +9,69 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
+
+var mergeTwoSortedLists = function(list1, list2){
+
+  var work;
+
+  if (list1 === null){
+    return list2;
+  } else if (list2 === null){
+    return list1;
+  }
+
+  if (list1.val < list2.val){
+    work = list1;
+    list1 = list1.next;
+  } else {
+    work = list2;
+    list2 = list2.next;
+  }
+
+  var root = work;
+
+
+  while (list1 !== null || list2 !== null){
+    if (list1 === null){
+      work.next = list2;
+      break;
+    } else if (list2 === null){
+      work.next = list1;
+      break;
+    }
+
+    if (list1.val < list2.val){
+      work.next = list1;
+      list1 = list1.next;
+      work = work.next;
+    } else {
+      work.next = list2;
+      list2 = list2.next;
+      work = work.next;
+    }
+  }
+
+
+  return root;
+} 
+
+
+
 var mergeKLists = function(lists) {
-    
+  if (lists[0] === undefined){
+    return [];
+  }
+  var result = lists[0];
+
+  for (var i = 1; i < lists.length; i++){
+    result = mergeTwoSortedLists(result, lists[i]);
+  }
+  return result;
 };
+
+var test = [{"val":1,"next":{"val":2,"next":{"val":2,"next":null}}},{"val":1,"next":{"val":1,"next":{"val":2,"next":null}}}];
+
+var oneList = {"val":13,"next":{"val":15,"next":{"val":21,"next":null}}};
+var twoList = {"val":1,"next":{"val":14,"next":{"val":20,"next":null}}};
+
+mergeTwoSortedLists(oneList, twoList);
