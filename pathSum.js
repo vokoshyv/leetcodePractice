ffce7946ce13486b0e057fab7a170eddf7c88d4a
vokoshyv <ronsuij@gmail.com>
2015-09-14 20:37:35 -0700

    a bit of fumbling around with pathSum.js

--- pathSum.js
+++ pathSum.js
@@ -11,5 +11,27 @@
  * @return {boolean}
  */
 var hasPathSum = function(root, sum) {
-    
+  var result = false;
+
+  if (root === null){
+    return false;
+  }
+
+  var search = function(currentSum, currentNode){
+    if (currentSum === sum && currentNode === null){
+      result = true;
+      return;
+    } else if (currentNode === null){
+      return;
+    } else if (currentSum === sum && currentNode.right === null && currentNode.left === null){
+      result = true;
+      return;
+    }
+
+    search(currentSum + currentNode.val, currentNode.right);
+    search(currentSum + currentNode.val, currentNode.left);
+  }
+  search(0, root);
+
+  return result;
 };
