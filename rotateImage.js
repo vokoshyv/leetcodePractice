114a6efdd595e0eb4b2a66453cfb942adfde3176
vokoshyv <ronsuij@gmail.com>
2015-09-14 21:23:29 -0700

    implements solution for rotateImage.js

--- rotateImage.js
+++ rotateImage.js
@@ -2,6 +2,18 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
+
 var rotate = function(matrix) {
-    
+  var copy = [];
+
+  matrix.map(function(row){
+    copy.push(row.slice());
+  })
+
+  for (var rowIndex = 0; rowIndex < matrix.length; rowIndex++){
+    for (var colIndex = 0; colIndex < matrix.length; colIndex++){
+      matrix[rowIndex][colIndex] = copy[matrix.length-1-colIndex][rowIndex];
+    }
+  }
+
 };
