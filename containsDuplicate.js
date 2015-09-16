1d2df0fc34135ca01cb7e501b9cc79c3e53597d9
vokoshyv <ronsuij@gmail.com>
2015-09-16 13:20:58 -0700

    implements solution for containsDuplicate.js

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -3,5 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    
+  var lib = {};
+
+  for (var i = 0; i < nums.length; i++){
+    if (lib[nums[i]] !== undefined){
+      return true;
+    }
+    lib[nums[i]] = true;
+  }
+  return false;
 };
