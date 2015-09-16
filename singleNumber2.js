fa4ffd9f6cb0a2623d72844108ff5ca5ead18e18
vokoshyv <ronsuij@gmail.com>
2015-09-16 15:48:18 -0700

    implements solution for singleNumber2.js

--- singleNumber2.js
+++ singleNumber2.js
@@ -3,5 +3,18 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    
+  var lib = {};
+
+  for (var i = 0; i < nums.length; i++){
+    if (lib[nums[i]] === undefined){
+      lib[nums[i]] = 1;
+    } else {
+      lib[nums[i]]++;
+      if (lib[nums[i]] === 3){
+        delete lib[nums[i]];
+      }
+    }
+  }
+
+  return Number(Object.keys(lib)[0]);
 };
