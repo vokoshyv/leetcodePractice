793baa78cdb3c26a9f42c177c486cfddbaa536da
vokoshyv <ronsuij@gmail.com>
2015-09-16 16:02:45 -0700

    implements solution for subsets.js

--- subsets.js
+++ subsets.js
@@ -3,5 +3,24 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-    
+  var work = [];
+
+  nums = nums.sort(function(a, b){
+    return a-b;
+  });
+
+  var findSubs = function(buildUp, depth){
+    if (depth === nums.length){
+      work.push(buildUp.slice());
+      return;
+    }
+
+    findSubs(buildUp, depth + 1);
+    buildUp.push(nums[depth]);
+    findSubs(buildUp, depth + 1);
+    buildUp.pop();
+  }
+
+  findSubs([], 0);
+  return work;
 };
