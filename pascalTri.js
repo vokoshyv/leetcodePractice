bcecbd9c811e927eec0de52edca5efb0c16d30ef
vokoshyv <ronsuij@gmail.com>
2015-09-23 10:07:06 -0700

    implements solution for pascalTri.js

--- pascalTri.js
+++ pascalTri.js
@@ -3,5 +3,20 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    
+  var result = [];
+
+  var work = [1];
+
+  for (var i = 0; i < numRows; i++){
+    result.push(work.slice());
+    var temp = [1];
+    for (var j = 0; j < work.length-1; j++){
+      temp.push(work[j] + work[j+1]);
+    }
+    temp.push(1);
+    work = temp;
+  }
+
+  return result;
+
 };
