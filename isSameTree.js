8d3d602c638e843e28d38246e77fa29ee869c44f
vokoshyv <ronsuij@gmail.com>
2015-09-24 10:24:21 -0700

    implements solution for isSameTree.js

--- isSameTree.js
+++ isSameTree.js
@@ -11,5 +11,29 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    
+
+  var result = true;
+
+  var checkTree = function(tree1, tree2){
+
+    if (tree1 === null && tree2 === null){
+      return;
+    } else if (tree1 === null && tree2 !== null){
+      result = false;
+      return;
+    } else if (tree2 === null && tree1 !== null){
+      result = false;
+      return;
+    } else if (tree1.val !== tree2.val){
+      result = false;
+      return;
+    } else if (tree1.val === tree2.val){
+      checkTree(tree1.right, tree2.right);
+      checkTree(tree1.left, tree2.left);
+    }
+  }
+
+  checkTree(p, q);
+
+  return result;
 };
