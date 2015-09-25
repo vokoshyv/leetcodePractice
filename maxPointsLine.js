454cd5e137204365ae0d4b652971f16c42625c47
vokoshyv <ronsuij@gmail.com>
2015-09-25 12:16:57 -0700

    initializes file for maxPointsLine.js

---
+++ maxPointsLine.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for a point.
+ * function Point(x, y) {
+ *     this.x = x;
+ *     this.x = y;
+ * }
+ */
+/**
+ * @param {Point[]} points
+ * @return {number}
+ */
+var maxPoints = function(points) {
+    
+};
