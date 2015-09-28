0b4157075e007449e7f9d356dc1c2293d385e5f2
vokoshyv <ronsuij@gmail.com>
2015-09-28 11:15:51 -0700

    initializes file for mergeSortedArray.js

---
+++ mergeSortedArray.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    
+};
