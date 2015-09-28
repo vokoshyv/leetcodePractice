66fae8b3863c4f936d674d965ca39a8f8b0b7ea2
vokoshyv <ronsuij@gmail.com>
2015-09-28 11:29:38 -0700

    have semi solution, but not quite working with leetcode

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -6,5 +6,32 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    
+  var work = [];
+  var mPointer = 0;
+  var nPointer = 0;
+  while (mPointer < m || nPointer < n){
+
+    if (mPointer === m){
+      work = work.concat(nums2.slice(nPointer));
+      break;
+    } else if (nPointer === n){
+      work = work.concat(nums1.slice(mPointer));
+      break;
+    } else if (nums1[mPointer] < nums2[nPointer]){
+      work.push(nums1[mPointer]);
+      mPointer++;
+      continue;
+    } else {
+      work.push(nums2[nPointer]);
+      nPointer++;
+      continue;
+    }
+  }
+
+  nums1 = work;
 };
+
+
+
+
+merge([0], 0, [1], 1);
