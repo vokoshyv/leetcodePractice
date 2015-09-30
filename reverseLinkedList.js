96d6e92d4c56eca1e5808614335345f41acaa122
vokoshyv <ronsuij@gmail.com>
2015-09-30 11:51:56 -0700

    issue with leetcode Test with incorrect test inputs

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -10,5 +10,31 @@
  * @return {ListNode}
  */
 var reverseList = function(head) {
-    
+
+  if (head === null){
+    return null;
+  }
+  if (head.next = null){
+    return head;
+  }
+
+  var temp = head;
+  var futureNext;
+
+  futureNext = head.next;
+console.log(head);
+console.log(futureNext === null);
+
+  while (futureNext !== null){
+    temp = futureNext;
+    temp.next = head;
+    console.log('indoor: ', futureNext);
+    futureNext = futureNext.next;
+    head = temp;
+  }
+  console.log("TEMP: ", temp);
+  console.log("FUTURENEXT: ", futureNext);
+  console.log("HEAD: ", head);
+  return temp;
 };
+
