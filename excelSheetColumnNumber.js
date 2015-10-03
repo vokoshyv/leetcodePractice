ab801683a5d68febab42130b45420c8d1fd4e5e5
Ron Tsui <ronsuij@gmail.com>
2015-10-03 15:00:18 -0700

    implements solution for excelSheetColumnNumber.js

--- excelSheetColumnNumber.js
+++ excelSheetColumnNumber.js
@@ -2,6 +2,20 @@
  * @param {string} s
  * @return {number}
  */
+
+var giveValueOfLetter = function(input){
+  return input.charCodeAt(0) - 64;
+}
+
 var titleToNumber = function(s) {
-  
+  var i = s.length; 
+  var work = 0;
+  var multiplier = 1;
+
+  while (i--){
+    work += multiplier * giveValueOfLetter(s[i]);
+    multiplier *= 26;
+  }
+
+  return work;
 };
