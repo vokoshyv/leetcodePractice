0d3c088766b532e6ba9e934428fbd4e22a87aa50
Ron Tsui <ronsuij@gmail.com>
2015-10-05 16:56:53 -0700

    reinitializing file for maximumProductSubarray.js

--- maximumProductSubarray.js
+++ maximumProductSubarray.js
@@ -3,20 +3,5 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-  var ultimateMax = nums[0];
-  var currentPositiveMax = nums[0];
-  var currentNegativeMin = nums[0];
-  var prevPosMax;
-  var prevNegMax;
-
-  for (var i = 1; i < nums.length; i++){
-    prevPosMax = currentPositiveMax;
-    prevNegMax = currentNegativeMin;
-
-    currentPositiveMax = Math.max(Math.max(prevPosMax*nums[i], prevNegMax*nums[i]), nums[i]);
-    currentNegativeMin = Math.min(Math.min(prevPosMax*nums[i], prevNegMax*nums[i]), nums[i]);
-    ultimateMax = Math.max(ultimateMax, currentPositiveMax);
-  }
-
-  return ultimateMax;
+    
 };
