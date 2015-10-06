f21554fd617f3b2287cc8eb2b1a7ba489500083d
Ron Tsui <ronsuij@gmail.com>
2015-10-05 17:16:52 -0700

    implements solution for maximumProductSubarray.js

--- maximumProductSubarray.js
+++ maximumProductSubarray.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    
+  var ultimateMax = nums[0];
+  var currentMax = nums[0];
+  var currentMin = nums[0];
+  var prevMaxCheck;
+  var prevMinCheck;
+
+  for (var i = 1; i < nums.length; i++){
+    prevMaxCheck = currentMax;
+    prevMinCheck = currentMin;
+
+    currentMax = Math.max(Math.max(prevMaxCheck * nums[i], prevMinCheck * nums[i]), nums[i]);
+    currentMin = Math.min(Math.min(prevMaxCheck * nums[i], prevMinCheck * nums[i]), nums[i]);
+
+    ultimateMax = Math.max(ultimateMax, currentMax);
+
+  }
+
+  return ultimateMax;
+
 };
