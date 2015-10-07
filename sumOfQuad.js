e33f54ebca73ce62c6cb04e5588f9a2c9a83c661
Ron Tsui <ronsuij@gmail.com>
2015-10-07 12:32:52 -0700

    initializes file for sumOfQuad.js

---
+++ sumOfQuad.js
@@ -0,0 +1,7 @@
+// Given a matrix of random numbers, find the sum of any 
+// selected quadrant in constant time. 
+
+// Time Complexity: Constant-time
+// Space Complexity: Linear
+// Pre-processing: Allowed
+
