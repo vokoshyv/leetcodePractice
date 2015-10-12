2033b9e1750105bd9b98de04927502e531896c44
Ron Tsui <ronsuij@gmail.com>
2015-10-12 12:16:59 -0700

    funky case with huge numbers

--- moveZeroes.js
+++ moveZeroes.js
@@ -3,5 +3,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    
+  nums.sort(function(a, b){
+    if (a === 0 && b !== 0){
+      return 1;
+    } else {
+      return 0;
+    }
+  })
 };
