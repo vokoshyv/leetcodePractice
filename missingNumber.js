9a35c5a32ef1898b01f878a5087f18b4f40058fb
vokoshyv <ronsuij@gmail.com>
2015-10-16 11:01:50 -0700

    implements faster solution

--- missingNumber.js
+++ missingNumber.js
@@ -3,22 +3,15 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-  if (nums.length === 1){
-    return (nums[0] === 1)? 0 : 1;
-  }
   var lib = [];
   nums.forEach(function(value){
     lib[value] = true;
   })
-  console.log(lib);
 
   for (var i = 0; i < lib.length; i++){
     if (!lib[i]){
-      console.log(i);
       return i;
     }
   }
   return lib.length;
 };
-
-missingNumber([0, 1, 3, 4, 5]);
