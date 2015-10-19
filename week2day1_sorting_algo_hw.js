41b5153f157d73c9efdd974f41a78dc534df4a45
Ron Tsui <ronsuij@gmail.com>
2015-10-18 21:38:26 -0700

    initializes file for week2day1_sorting_algo_hw.js

---
+++ week2day1_sorting_algo_hw.js
@@ -0,0 +1,50 @@
+ /********************************************************************** 
+  *                          Homework III                              *
+  *                                                                    *
+  *  Problem: Insertion Sort                                           *
+  *                                                                    *
+  *  Prompt: Given an unsorted array of numbers,                       *
+  *          return a sorted array using insertion sort.               *
+  *                                                                    *
+  *  Input:  An unsorted array                                         *
+  *  Output: A sorted array                                            *
+  *                                                                    *
+  *  Example: input = [3,9,1,4,7] , output = [1,3,4,7,9]               *
+  *                                                                    *
+  *  What are the time and auxilliary space complexity?                *
+  *                                                                    *
+  **********************************************************************/
+
+
+
+
+
+
+
+  /**********************************************************
+   *             Highly Recommended Extra Credit            *
+   *                                                        *
+   *  Problem: Selection Sort                               *
+   *                                                        *
+   *  Prompt: Given an unsorted array of numbers,           *
+   *          return a sorted array using insertion sort.   *
+   *                                                        *
+   *  Input: An unsorted array                              *
+   *  Output: A sorted array                                *
+   *                                                        *
+   *  Example: input = [8,3,2,10] output = [2,3,8,10]       *
+   *                                                        *
+   *  What are the time and auxilliary space complexity?    *
+   *  What is the best case time complexity?                *
+   *                                                        *
+   **********************************************************/
+
+
+
+var insertionSort = function(input){
+  var work = input;
+
+  var compareShift = function(indexToCompare){
+    while work
+  }
+}
