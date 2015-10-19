a02f0c0ddc26cfdcf1586da62ba01dba7141b289
Ron Tsui <ronsuij@gmail.com>
2015-10-18 22:20:40 -0700

    readies insertion and selection sort

--- week2day1_sorting_algo_hw.js
+++ week2day1_sorting_algo_hw.js
@@ -41,10 +41,87 @@
 
 
 
-var insertionSort = function(input){
-  var work = input;
+var insertionSort = function(work){
 
-  var compareShift = function(indexToCompare){
-    while work
+  var compareAndShift = function(indexToCompare){
+    while (indexToCompare > 0 && work[indexToCompare] < work[indexToCompare-1]){
+      var temp = work[indexToCompare];
+      work[indexToCompare] = work[indexToCompare-1];
+      work[indexToCompare-1] = temp;
+      indexToCompare--;
+    }
   }
+
+  for (var i = 0; i < work.length; i++){
+    compareAndShift(i);
+  }
+
+  return work;
 }
+
+// insertionSort([5, 4, 3, 2, 1, 1, 1]);
+
+// Time Complexity: 
+// Best Case: linear Ω(n); 
+// Worst Case (Big O): Intuitively, quadratic, 
+// but we didn't get to the full length of the array until the last comparison. It looks like 
+// 1 + 2 + 3 + ... operations, which is a summation. This comes out to (n(n-1))/2, which is 
+// n^2/2 - n/2. The n^2 dominates, so we can consider it O(n^2). 
+
+// Space Complexity: Constant Space Complexity; all modification occurs on the input
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var selectionSort = function(work){
+
+  var findMinimumAndSwitch = function(indexToReplace){
+    var minimum = work[indexToReplace];
+    var minimumIndex = indexToReplace;
+    for (var i = indexToReplace+1; i < work.length; i++){
+      if (work[i] < minimum){
+        minimum = work[i];
+        minimumIndex = i;
+      }
+    }
+    work[minimumIndex] = work[indexToReplace];
+    work[indexToReplace] = minimum;
+  }
+
+  for (var i = 0; i < work.length; i++){
+    findMinimumAndSwitch(i);
+  }
+
+  return work;
+}
+
+selectionSort([2, 3, 4, 1, 1, 1, 1]);
+
+// Time Complexity: 
+
+// Worst Case: Same as insertion sort: 1 + 2 + 3 + ... operations. So it will come out to 
+// n^2/2 - n/2. The n^2 dominates, so it is O(n^2). 
+// Best Case: Same thing because we still need to check for the minimum of each unsorted portion
+// with every run, so Ω(n^2). 
+
+// Space Complexity: Constant Space Complexity; all modifications occur on the input
+
