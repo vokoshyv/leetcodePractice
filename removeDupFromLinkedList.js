d6d31498532cab350768d43ca9278eea150eb62c
vokoshyv <ronsuij@gmail.com>
2015-10-22 11:02:45 -0700

    initializes file for removeDupFromLinkedList.js

---
+++ removeDupFromLinkedList.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    
+};
