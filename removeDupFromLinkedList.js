b7f14b0694468d9170b2b9350fa50f84eaa9a22a
vokoshyv <ronsuij@gmail.com>
2015-10-22 11:29:48 -0700

    implements faster solution

--- removeDupFromLinkedList.js
+++ removeDupFromLinkedList.js
@@ -1,30 +1,7 @@
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
-
 /**
  * @param {ListNode} head
  * @return {ListNode}
  */
-
-// var linkedList = function(){
-//   this.head = null;
-//   this.tail = null;
-//   this.size = 0;
-// }
-
-// linkedList.prototype.append = function(value){
-//   if (this.size === 0){
-//     this.head = new ListNode(value);
-//     this.tail = this.head;
-//     this.size++;
-//   } else {
-//     this.tail.next = new ListNode(value);
-//     this.tail = this.tail.next;
-//     this.size++;
-//   }
-// }
 
 var deleteDuplicates = function(head) {
   if (head === null){
@@ -32,17 +9,15 @@ var deleteDuplicates = function(head) {
   }
 
   var work = head;
-  // var storage = new linkedList();
-  var reference = work;
+  var reference = work.val;
   var result = [];
   result.push(work.val);
-  // storage.append(work.val);
 
   work = work.next;
   while (work !== null){
-    if (reference.val !== work.val){
+    if (reference !== work.val){
       result.push(work.val);
-      reference = work;
+      reference = work.val;
     }
     work = work.next;
   }
