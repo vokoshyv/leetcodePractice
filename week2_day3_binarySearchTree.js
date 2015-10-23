e56a15b5d0e9a1fe0e4b980d3b4e7571e2d3d8a4
Ron Tsui <ronsuij@gmail.com>
2015-10-23 12:49:18 -0700

    initializes file

---
+++ week2_day3_binarySearchTree.js
@@ -0,0 +1,54 @@
+/*********************************************************************************** 
+  *                                 Homework V                                     *
+  *                                                                                *
+  *  Problem: Binary Search Tree                                                   *
+  *                                                                                *
+  *  Prompt: Create a BinarySearchTree class/constructor.                          *
+  *          Name it binarySearchTree (js) or binary_search_tree (rb/py).          *
+  *                                                                                *
+  *          Part 1: Create a node class for your binarySearchTree.                *
+  *                  Your node class will take an integer value and output         *
+  *                  an object with the following properties:                      *
+  *                                                                                *
+  *                  node.value: input value                                       *
+  *                  node.leftChild: a pointer to the left child Node              * 
+  *                  node.rightChild: a pointer to the right child Node            *
+  *                                                                                *
+  *                  Example: { value: 1, leftChild: null, rightChild: null }      *
+  *                                                                                *
+  *          Part 2: Create the BinarySearchTree class.                            *
+  *                  It should contain the following properties:                   *
+  *                                                                                *
+  *                  root: pointer to the root node                                *
+  *                  size: number of nodes in the BinarySearchTree                 *
+  *                                                                                *
+  *                  The BinarySearchTree will also have the following properties: *
+  *                                                                                *
+  *                  insert: method that takes takes an input value, and creates a *
+  *                          new node with the given input.  The method will then  *
+  *                          find the correct place to add the new node. (Remember *
+  *                          that nodes with values larger than the parent node go *
+  *                          to the right, and smaller values go to the left.)     *
+  *                                                                                *
+  *                  search: method that will search to see if a node with a       *
+  *                          specified value exists.  If present returns true,     *
+  *                          else returns false.                                   *
+  *                                                                                *
+  *  Input:  N/A                                                                   *
+  *  Output: A BinarySearchTree instance                                           *
+  *                                                                                *
+  *  What are the time and auxilliary space complexities of the various methods?   *
+  *                                                                                *
+  **********************************************************************************/
+
+
+ /**
+  *  Extra Credit: Remove
+  *
+  *  Prompt: Create a remove method on your BinarySearchTree that will remove and
+  *          return a given value from the tree and retie the tree so it remains
+  *          properly sorted.
+  **/
+
+
+
