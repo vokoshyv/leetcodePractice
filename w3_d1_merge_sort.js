5df8d9f9593e9a5a9b4442f360c3a30793dd316e
Ron Tsui <ronsuij@gmail.com>
2015-10-25 18:35:05 -0700

    implements w3_d1_merge_sort.js

--- w3_d1_merge_sort.js
+++ w3_d1_merge_sort.js
@@ -1,7 +1,7 @@
 var sorted1 = [1, 3, 5, 7];
 var sorted2 = [2, 4, 6, 8, 10, 12, 14];
 
-var test = [5, 7, 2, 6, 8, 3];
+var test = [5, 4, 3, 2, 1, 0, -1, -2];
 
 
 // function for joining two sorted arrays together
@@ -35,10 +35,28 @@ var joinArrays = function(arr1, arr2){
 
 var mergeSort = function(input){
   var work = [];
+  
   input.forEach(function(value){
     work.push([value]);
   })
 
+  while (work.length !== 1){
+    var pointer = 0;
+    sorted = [];
 
+    while (pointer < work.length){
+      if (work[pointer+1] !== undefined){
+        sorted.push(joinArrays(work[pointer], work[pointer+1]));
+      } else {
+        sorted.push(work[pointer]);
+      }
+      pointer += 2;
+    }
+
+    work = sorted;
+  }
+
+  return work[0];
 }
 
+mergeSort(test);
