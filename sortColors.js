b93028fb9c4f48d7c446738c412c42682b812860
vokoshyv <ronsuij@gmail.com>
2015-10-26 15:12:29 -0700

    some work on sortcolors.js

--- sortColors.js
+++ sortColors.js
@@ -3,5 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function(nums) {
+  var startPointer = 0;
+  var endPointer = nums.length-1;
+
+  while (startPointer < endPointer){
     
+  }
+
+
+  
 };
