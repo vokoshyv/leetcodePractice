0799b8e5dff2b693ca9d60e97257ebdc41b924e4
vokoshyv <ronsuij@gmail.com>
2015-10-26 17:33:27 -0700

    adds prompt to problem

--- w3_d1_quick_sort.js
+++ w3_d1_quick_sort.js
@@ -1,9 +1,28 @@
+ /********************************************************************** 
+  *                          Homework VI                               *
+  *                                                                    *
+  *  Problem: Quicksort                                                *
+  *                                                                    *
+  *  Prompt: Given an unsorted array of numbers,                       *
+  *          return a sorted array using Quicksort sort.               *
+  *                                                                    *
+  *  Input:  An unsorted array                                         *
+  *  Output: A sorted array                                            *
+  *                                                                    *
+  *  Example: input = [3,9,1,4,7] , output = [1,3,4,7,9]               *
+  *                                                                    *
+  *  What are the time and auxilliary space complexity?                *
+  *                                                                    *
+  **********************************************************************/
+
+
+
 
 
 // make array full of a million random numbers
-
 var i = 1000000;
 var test = [];
+
 while (i--){
     test.push(Math.floor(1000000 * Math.random()));
 }
