74e05a560ba40816984dfe6793a9fbdcfe679c60
Ron Tsui <ronsuij@gmail.com>
2015-11-17 20:23:39 -0800

    a brute solution

--- symmetricTree.js
+++ symmetricTree.js
@@ -10,5 +10,32 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    
+    var right = '';
+    var left = '';
+
+    var rightFirst = function(current){
+      if (current === null){
+        right += 'null'
+        return;
+      }
+
+      right += current.val;
+
+      rightFirst(current.right);
+      rightFirst(current.left);
+    }
+
+    var leftFirst = function(){
+      if (current === null){
+        left += 'null'
+        return;
+      }
+
+      left += current.val;
+
+      leftFirst(current.left);
+      leftFirst(current.right);
+    }
+
+    return (right === left);
 };
