6cd78a5321b87c008e4259e1c186a8d43cf9565b
Ron Tsui <ronsuij@gmail.com>
2015-11-27 16:09:03 -0800

    return to this later

--- wordPattern.js
+++ wordPattern.js
@@ -4,5 +4,37 @@
  * @return {boolean}
  */
 var wordPattern = function(pattern, str) {
-  
+  var work = str.split(' ');
+
+  var lastTwo = function(input){
+    return input.slice(input.length-2);
+  }
+
+  var lib = {};
+
+  pattern.split('').forEach(function(letter, index){
+    if (lib[letter] === undefined){
+      lib[letter] = [index];
+    } else {
+      lib[letter].push(index);
+    }
+  })
+
+  console.log(lib);
+  var check = true;
+
+  Object.keys(lib).forEach(function(letter){
+    console.log(letter);
+    var temp = lastTwo(work[lib[letter][0]]);
+    console.log('haha');
+
+    for (var i = 1; i < lib[letter].length; i++){
+      if (lastTwo(work[lib[letter][i]]) !== temp){
+        check = false;
+      }
+    }
+
+  })
+
+  return check;
 };
