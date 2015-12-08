1e4679a71d7da3d7782c39c81d4c9199d756c71f
Ron Tsui <ronsuij@gmail.com>
2015-12-08 10:30:24 -0800

    initializes file for lowestCommonAncestor.js

---
+++ lowestCommonAncestor.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    
+};
