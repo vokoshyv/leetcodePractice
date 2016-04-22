fe25afca17747ae7b8b70e98177fa2f84735ca4e
vokoshyv <ronsuij@gmail.com>
2016-04-22 14:52:28 -0700

    (feat) implements solution for reverseString.js

--- islands.js
+++ islands.js
@@ -1,7 +1,37 @@
 /**
- * @param {character[][]} grid
- * @return {number}
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ *
+ * 
+ * Example 1:
+ * 
+ * [[1,1,1,1,0],
+ *  [1,1,0,1,0],
+ *  [1,1,0,0,0],
+ *  [0,0,0,0,0]]
+ * Answer: 1
+ * 
+ * Example 2:
+ * 
+ * [[1,1,0,0,0],
+ *  [1,1,0,0,0],
+ *  [0,0,1,0,0],
+ *  [0,0,0,1,1]]
+ * Answer: 3
+ *
+ * 
  */
+
+
+
+
+
+
+
+
+
+
+
+
 var numIslands = function(grid) {
   var traveledTo = [];
   var i = grid.length; 
