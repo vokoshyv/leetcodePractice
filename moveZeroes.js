da42e1b3d34fc18c62424d122c89e72e3bf749fc
vokoshyv <ronsuij@gmail.com>
2016-04-22 15:14:10 -0700

    (feat) implements solution for moveZeroes.js

--- moveZeroes.js
+++ moveZeroes.js
@@ -3,11 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-  nums.sort(function(a, b){
-    if (a === 0){
-      return 1;
-    } else {
-      return 0;
+  var temp;
+  var toHere = nums.length;
+  var i = nums.length; 
+  var pointer;
+
+  while (i--){
+    if (nums[i] === 0){
+      pointer = i;
+      while (pointer < nums.length-1 && pointer < toHere-1){
+        nums[pointer] = nums[pointer+1];
+        pointer++;
+      }
+      nums[pointer] = 0;
     }
-  });
+  }
 };
