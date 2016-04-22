f4c67f6eaae3d3109a3f5c30484b0a0eb6057c1a
vokoshyv <ronsuij@gmail.com>
2016-04-22 15:32:38 -0700

    (feat) initializes file for reverseSinglyLinkedList.js

---
+++ reverseSinglyLinkedList.js
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if (head === null){
+    return head;
+  } else if (head.next === null){
+    return head;
+  }
+
+  var temp = reverseList(head.next);
+  temp.next = head;
+  return temp;
+};
+
+
+
+
+// 1 --> 5 --> 10 --> 15
+
+
+// 1 --> null
+
+// 1 --> 5 --> null
+
+
+
+
+
+
+
+
+
+
+
