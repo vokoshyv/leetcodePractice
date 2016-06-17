426973bb5387b1ee22e185e69ea81c4e7a17771a
vokoshyv <ronsuij@gmail.com>
2016-06-17 15:34:13 -0700

    (feat) restarting productOfArrayExceptSelf.js

--- productOfArrayExceptSelf.js
+++ productOfArrayExceptSelf.js
@@ -12,23 +12,5 @@
  * @return {number[]}
  */
 var productExceptSelf = function(arr) {
-  var work = [];
-  var checkFor = 1;
-  var checkBack = 1;
-  var size = arr.length;
-
-  for (var i = 1; i < arr.length; i++){
-    if (work[i] === undefined){
-      work[i] = 1;
-    };
-    if (work[size-i-1] === undefined){
-      work[size-i-1] = 1;
-    }
-    work[i] *= (arr[i-1] * checkFor);
-    work[size-i-1] *= (arr[size-i] * checkBack);
-    checkFor *= arr[i-1];
-    checkBack *= arr[size-i];
-  }
-
-  return work;
+  
 };
