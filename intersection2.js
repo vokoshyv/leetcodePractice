dff2459933b0c3fd3fccb54da1c153c5e384096d
vokoshyv <ronsuij@gmail.com>
2016-06-20 15:32:10 -0700

    (feat) implements solution for intersection2.js

--- intersection2.js
+++ intersection2.js
@@ -7,6 +7,24 @@ var intersect = function(nums1, nums2) {
   var lib = {};
 
   nums1.forEach(function(element){
-    lib[element] = true;
+    if (lib[element] === undefined){
+      lib[element] = 1;
+    } else {
+      lib[element]++;
+    }
   })
+
+  var result = [];
+
+  nums2.forEach(function(element){
+    if (lib[element]){
+      result.push(element);
+      lib[element]--;
+      if (lib[element] === 0){
+        delete lib[element];
+      }
+    }
+  })
+
+  return result;
 };
