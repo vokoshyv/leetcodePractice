4ba0b28d6e4ec9d49cafc9c6d9fb948988df071f
vokoshyv <ronsuij@gmail.com>
2016-06-20 15:28:58 -0700

    (feat) initializes file for intersection2.js

---
+++ intersection2.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+  var lib = {};
+
+  nums1.forEach(function(element){
+    lib[element] = true;
+  })
+};
