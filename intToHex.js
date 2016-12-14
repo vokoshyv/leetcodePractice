494592a80ecad12b0f132bce1b113c5be257c93e
vokoshyv <ronsuij@gmail.com>
2016-12-14 11:28:36 -0800

    (feat) implements solution for intToHex.js

---
+++ intToHex.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var toHex = function(num) {
+  var lib = '0123456789abcdef';
+  var result = '';
+  if (num < 0){
+    num = 4294967296 + num;
+  }
+  
+  while (num > 15){
+    result = lib[num%16] + result;
+    num = Math.floor(num / 16);
+  }
+  result = lib[num] + result;
+  return result;
+};
+
