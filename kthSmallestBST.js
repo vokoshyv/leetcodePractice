9c27d2b1a24bbbd0b23cbd0e04e5dc5323a3180f
vokoshyv <ronsuij@gmail.com>
2016-12-25 23:19:33 -0800

    (feat) implements solution for kthSmallestBST.js

--- kthSmallestBST.js
+++ kthSmallestBST.js
@@ -11,5 +11,18 @@
  * @return {number}
  */
 var kthSmallest = function(root, k) {
-    
+  var results = [];
+  function traverse(cNode){
+    if (cNode === null){
+      return;
+    } else if (results.length > k){
+      return;
+    }
+
+    traverse(cNode.left);
+    results.push(cNode.val);
+    traverse(cNode.right);
+  }
+  traverse(root);
+  return results[k - 1];
 };
