a339b4807d4ebf6284d56be111ee3a90c04627b1
vokoshyv <ronsuij@gmail.com>
2016-12-25 23:03:11 -0800

    (feat) initializes file for kthSmallestBST.js

---
+++ kthSmallestBST.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    
+};
