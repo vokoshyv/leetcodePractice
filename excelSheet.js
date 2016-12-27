f3a30f51137f636347b950ca981c2adb33b2186d
vokoshyv <ronsuij@gmail.com>
2016-12-26 16:24:24 -0800

    (feat) implements solution for excelSheet.js

--- excelSheet.js
+++ excelSheet.js
@@ -3,5 +3,14 @@
  * @return {string}
  */
 var convertToTitle = function(n) {
-    
+  var lib = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var result = '';
+  n--;
+  while (n > -1){
+    result = lib[Math.floor(n % 26)] + result;
+    n = Math.floor(n / 26);
+    n--;
+  }
+
+  return result;
 };
