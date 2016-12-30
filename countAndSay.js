4f60f5b9517bff5bd236ad552064720c5da5a3f8
vokoshyv <ronsuij@gmail.com>
2016-12-30 11:18:18 -0800

    (feat) implements solution for countAndSay.js

--- countAndSay.js
+++ countAndSay.js
@@ -3,5 +3,29 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-    
+  var result = '1';
+  var temp;
+  var count;
+  var previous;
+
+  while (--n){
+    temp = '';
+    count = 1;
+    previous = result[0];
+    for (var i = 1; i < result.length; i++){
+      if (result[i] === previous){
+        count++;
+      } else {
+        temp += count;
+        temp += previous;
+        count = 1;
+        previous = result[i];
+      }
+    }
+    temp += count;
+    temp += previous;
+    result = temp;
+  }
+
+  return result;
 };
