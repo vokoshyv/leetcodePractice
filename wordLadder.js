7f23c6c4681f476ac464ef057257b9ba81f4f01a
vokoshyv <ronsuij@gmail.com>
2017-01-08 16:46:19 -0800

    (feat) initializes wordLadder.js file

---
+++ wordLadder.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {Set} wordList
+ *   Note: wordList is a Set object, see:
+ *   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+ * @return {number}
+ */
+var ladderLength = function(beginWord, endWord, wordList) {
+    
+};
