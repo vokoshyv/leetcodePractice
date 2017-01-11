22f634c79cdcebd4559822b46bcb57a4bf9fc542
vokoshyv <ronsuij@gmail.com>
2017-01-11 10:20:16 -0800

    (feat) initializes file for recoverBST.js

---
+++ recoverBST.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var recoverTree = function(root) {
+    
+};
