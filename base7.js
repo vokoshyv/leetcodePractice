3699e31acc99ba07faca42c5842860a9d2d78c8e
vokoshyv <ronsuij@gmail.com>
2017-04-02 17:26:19 -0700

    (feat) implements solution for base7.js

--- base7.js
+++ base7.js
@@ -3,5 +3,19 @@
  * @return {string}
  */
 var convertToBase7 = function(num) {
-    
+  var strNum = num.toString();
+  var isNeg = (strNum[0] === '-')? true : false;
+  var justNum = (isNeg)? strNum.slice(1) : strNum;
+
+  function build(num){
+    if (num < 7){
+      return '0123456'[num]
+    }
+
+    return build(Math.floor(num / 7)) + '0123456'[num % 7];
+  }
+
+  var base7 = build(Number(justNum));
+
+  return (isNeg)? '-' + base7 : base7;
 };
