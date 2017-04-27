b5a281856867a2f16843053c98e10d1525e96a2a
vokoshyv <ronsuij@gmail.com>
2017-04-27 16:46:39 -0700

    (feat) implements solution for keyboardRow.js

--- keyboardRow.js
+++ keyboardRow.js
@@ -3,5 +3,31 @@
  * @return {string[]}
  */
 var findWords = function(words) {
-    
+  var top = 'qwertyuiop';
+  var middle = 'asdfghjkl';
+  var bottom = 'zxcvbnm';
+
+  var lib = {};
+  top.split('').forEach(function(letter){
+    lib[letter] = 1;
+  });
+  middle.split('').forEach(function(letter){
+    lib[letter] = 2;
+  });
+  bottom.split('').forEach(function(letter){
+    lib[letter] = 3;
+  });
+
+  var result = [];
+
+  return words.filter(function(word){
+    word = word.toLowerCase();
+
+    for (var i = 1; i < word.length; i++){
+      if (lib[word[i]] !== lib[word[i-1]]){
+        return false;
+      }
+    }
+    return true;
+  });
 };
