bcdfc3935c3c4af8d966db4b985d48583d381682
vokoshyv <ronsuij@gmail.com>
2017-05-06 07:57:23 -0700

    (feat) implements solution for maxConsecutiveOnes.js

--- maxConsecutiveOnes.js
+++ maxConsecutiveOnes.js
@@ -5,7 +5,9 @@
 var findMaxConsecutiveOnes = function(nums) {
   var maxOnes = 0;
   var onesCount = 0;
-  for (var i = 1; i < nums.length; i++){
-    if ()
-  }
+  nums.forEach(function(num){
+    num? onesCount++ : onesCount = 0;
+    maxOnes = Math.max(maxOnes, onesCount);
+  })
+  return maxOnes;
 };
