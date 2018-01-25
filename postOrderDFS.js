4f57077146ac260a2ed2818667b294608696e8cb
vokoshyv <ronsuij@gmail.com>
2018-01-24 20:27:41 -0800

    (feat) implements solution for postOrderDFS.js

--- postOrderDFS.js
+++ postOrderDFS.js
@@ -10,5 +10,31 @@
  * @return {number[]}
  */
 var postorderTraversal = function(root) {
+  if (root === null) {
+    return [];
+  }
+  let result = [];
+  let stack = [];
+  stack.push(root);
+  let visited = new Set();
 
+  while (stack.length > 0) {
+    let top = stack[stack.length - 1];
+    if (!visited.has(top)) {
+      if (top.right) {
+        stack.push(top.right);
+      }
+      if (top.left) {
+        stack.push(top.left);
+      }
+      visited.add(top);
+    } else {
+      top = stack.pop();
+      result.push(top.val);
+    }
+  }
+
+
+
+  return result;
 };
