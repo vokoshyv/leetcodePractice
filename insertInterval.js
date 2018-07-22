a51f52f4d02cc07b35d29a32961c3346e49fed69
vokoshyv <ronsuij@gmail.com>
2018-07-22 16:13:01 -0700

    (feat) initializes file for insertInterval.js

---
+++ insertInterval.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for an interval.
+ * function Interval(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ * }
+ */
+/**
+ * @param {Interval[]} intervals
+ * @param {Interval} newInterval
+ * @return {Interval[]}
+ */
+var insert = function(intervals, newInterval) {
+
+};
