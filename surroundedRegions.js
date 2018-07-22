4df8399eb929f31266928df24fdadbd2798b2d78
vokoshyv <ronsuij@gmail.com>
2018-07-22 16:03:08 -0700

    (feat) implements solution for surroundedRegions.js

--- surroundedRegions.js
+++ surroundedRegions.js
@@ -8,58 +8,70 @@ var solve = function(board) {
   }
   let board_y = board.length;
   let board_x = board[0].length;
-
+  let freedom = false;
   let visited = new Set();
 
   for (let y = 1; y < board_y - 1; y++) {
     for (let x = 1; x < board_x - 1; x++) {
       if (board[y][x] === "O") {
+        freedom = false;
+        visited = new Set();
         findOpen(x, y);
+        if (!freedom) {
+          visited.forEach(key=>{
+            key = key.split('_');
+            board[parseInt(key[1])][parseInt(key[0])] = 'X';
+          })
+        }
       }
     }
   }
 
 
   function findOpen(x, y) {
+    let key = x + '_' + y;
     if (board[y][x] === 'X') {
-      return false;
+      return;
     }
-    if (x === 0 || y === 0 || x >= board_x - 1 || y >= board_y - 1) {
+    if (visited.has(key)) {
+      return;
+    }
+    if (x === 0 || y === 0 || x >= (board_x - 1) || y >= (board_y - 1)) {
       if (board[y][x] === 'O') {
-        return true;
+        freedom = true;
+        return;
       } else {
-        return false;
+        return;
       }
     }
 
-    board[y][x] === 'X';
+    visited.add(key);
 
-    let up = findOpen(x, y - 1);
-    let down = findOpen(x, y + 1);
-    let left = findOpen(x - 1, y);
-    let right = findOpen(x + 1, y);
-
-    if (up || down || left || right) {
-      board[y][x] = 'O';
-      return true;
-    }
-
-    return false;
+    findOpen(x, y - 1);
+    findOpen(x, y + 1);
+    findOpen(x - 1, y);
+    findOpen(x + 1, y);
   }
 };
 
 
-let test = [["O","X","X"],
-            ["X","O","O"],
-            ["X","O","X"],
-            ["X","X","X"]]
+// let test = [["O","X","X"],
+//             ["X","O","O"],
+//             ["X","O","X"],
+//             ["X","X","X"]]
 
-console.log(test);
-solve(test);
+// let test = [["O","X","X","O","X"],
+//             ["X","X","X","X","O"],
+//             ["X","X","X","O","X"],
+//             ["O","X","O","O","O"],
+//             ["X","X","O","X","O"]]
 
-console.log("\n");
-
-console.log(test);
+// console.log(test);
+// solve(test);
+//
+// console.log("\n");
+//
+// console.log(test);
 /*
 [["O","X","X","O","X"],
  ["X","X","X","X","O"],
