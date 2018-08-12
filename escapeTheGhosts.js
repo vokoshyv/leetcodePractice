b3f503b69d70a86856df1f88e963e76187efa8f2
vokoshyv <ronsuij@gmail.com>
2018-08-12 13:05:06 -0700

    (feat) implements solution for escapeTheGhosts.js

--- escapeTheGhosts.js
+++ escapeTheGhosts.js
@@ -5,4 +5,13 @@
  */
 var escapeGhosts = function(ghosts, target) {
 
+  function findDistanceToTarget(startX, startY, targetX, targetY) {
+    return Math.abs(startX - targetX) + Math.abs(startY - targetY);
+  }
+
+  let myDistance = findDistanceToTarget(0, 0, target[0], target[1]);
+
+  return ghosts.map(ghost => findDistanceToTarget(ghost[0], ghost[1], target[0], target[1]))
+  .reduce((result, distance) => result && distance > myDistance, true);
+
 };
