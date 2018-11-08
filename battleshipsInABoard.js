de6697bd9099f3aacad62ce0f668a897babc04a1
vokoshyv <ronsuij@gmail.com>
2018-11-08 15:33:19 -0800

    (feat) implements solutino for battleshipsInABoard.js

--- battleshipsInABoard.js
+++ battleshipsInABoard.js
@@ -3,5 +3,26 @@
  * @return {number}
  */
 var countBattleships = function(board) {
+  let count = 0;
 
+  board.forEach((row, y) => {
+    row.forEach((item, x) => {
+      if (item === 'X') {
+        if (y === 0 && x === 0) {
+          count++;
+        } else if (y === 0) {
+          if (board[y][x-1] === '.') {
+            count++;
+          }
+        } else if (x === 0) {
+          if (board[y-1][x] === '.') {
+            count++;
+          }
+        } else if (board[y-1][x] === '.' && board[y][x-1] === '.'){
+          count++;
+        }
+      }
+    });
+  });
+  return count;
 };
