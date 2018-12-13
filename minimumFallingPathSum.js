6c42cbbf2c217e3fb3bd91cceedde0672c0d3a2a
vokoshyv <ronsuij@gmail.com>
2018-12-13 12:03:19 -0800

    (feat) implements solution for minimumFallingPathSum.js

--- minimumFallingPathSum.js
+++ minimumFallingPathSum.js
@@ -3,5 +3,28 @@
  * @return {number}
  */
 var minFallingPathSum = function(A) {
+  let y = 1;
 
+  while (y < A.length) {
+    for (let x = 0; x < A.length; x++) {
+      A[y][x] += findMinAbove(A, x, y);
+    }
+
+    if (y === A.length - 1) {
+      return Math.min(...A[y]);
+    }
+    y++;
+  }
+
+  return A[0][0];
 };
+
+function findMinAbove(A, x, y) {
+  if (x === 0) {
+    return Math.min(A[y-1][x], A[y-1][x+1]);
+  } else if (x === (A.length - 1)) {
+    return Math.min(A[y-1][x], A[y-1][x-1]);
+  } else {
+    return Math.min(A[y-1][x], A[y-1][x+1], A[y-1][x-1]);
+  }
+}
