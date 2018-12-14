6f24845383011172c39682610cfa18cd012db323
vokoshyv <ronsuij@gmail.com>
2018-12-13 17:44:04 -0800

    (feat) thinking through new idea with scrambleString.js

--- scrambleString.js
+++ scrambleString.js
@@ -4,61 +4,17 @@
  * @return {boolean}
  */
 var isScramble = function(s1, s2) {
-  let i = 0;
+  let words = new Set();
 
-  while (i < s1.length) {
-    let j = findIndexOfCharacter(s1.slice(i), s2.slice(i));
-    if (j < 0) {
-        return false;
+  function createScrambles(build, bank) {
+    if (build.length === s1.length) {
+      words.add(build);
+      return;
     }
 
-    let endIndex = i + j;
-
-    if (!checkEquivalency(i, endIndex, s1, s2)) {
-      return false;
-    }
-
-    i = endIndex + 1;
+    
   }
-  return true;
+  createScrambles("", s1);
+
+  return words.has(s2);
 };
-
-function findIndexOfCharacter(s1, s2) {
-  return Math.max(s1.indexOf(s2[0]), s2.indexOf(s1[0]));
-  // return substring.indexOf(char);
-}
-
-function checkEquivalency(startIndex, endIndex, s1, s2) {
-  let counts = {};
-
-  let i = startIndex;
-
-  while (i < endIndex + 1) {
-    if (counts[s1[i]] === undefined) {
-      counts[s1[i]] = 1;
-    } else {
-      counts[s1[i]] += 1;
-    }
-
-    i++;
-  }
-
-  i = startIndex;
-
-  while (i < endIndex + 1) {
-    if (counts[s2[i]] === undefined) {
-      return false;
-    } else {
-      counts[s2[i]] -= 1;
-      if (counts[s2[i]] === 0) {
-        delete counts[s2[i]];
-      }
-    }
-    i++;
-  }
-  return Object.keys(counts).length === 0;
-}
-
-
-
-console.log(isScramble("aa", "aa"));
