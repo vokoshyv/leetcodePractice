f5db0b7e7f3dad0a6e22f9aa992c7141990d4a1e
vokoshyv <ronsuij@gmail.com>
2018-12-19 17:49:24 -0800

    (feat) implements quadratic runtime solution for longestIncreasingSubsequence.js

--- longestIncreasingSubsequence.js
+++ longestIncreasingSubsequence.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 var lengthOfLIS = function(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  let result = [1];
 
+  for (let i = 1; i < nums.length; i++) {
+    let mostMax = 0;
+    let j = 0;
+
+    while (j < i) {
+      if (nums[j] < nums[i]) {
+        mostMax = Math.max(mostMax, result[j]);
+      }
+      j++;
+    }
+    result.push(mostMax + 1);
+  }
+
+  return Math.max(...result);
 };
