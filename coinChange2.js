ffe967f1a668cd0b4dc63da671e77cc158a5c2f9
vokoshyv <ronsuij@gmail.com>
2019-01-17 16:53:34 -0800

    (feat) implements solution for coinChange2.js

--- coinChange2.js
+++ coinChange2.js
@@ -4,5 +4,35 @@
  * @return {number}
  */
 var change = function(amount, coins) {
+  let work = new Array(amount + 1).fill(0);
+  work[0] = 1;
+  let workLength = work.length;
 
+  for (let coin of coins) {
+    let i = coin;
+
+    while (i < workLength) {
+      work[i] = work[i-coin] + work[i];
+      i++;
+    }
+  }
+
+  return work[work.length - 1];
 };
+
+
+console.log(change(10, [10]));
+
+
+// change(10, [1,2,3]);
+
+//  0 1 2 3 4
+// [1,0,0,0,0]
+// [1,1,1,1,1]
+// [1,1,2,2,3]
+// [1,1,2,3,4]
+//
+// 1,1,1,1
+// 1,1,2
+// 1,3
+// 2,2
