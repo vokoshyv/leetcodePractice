8fbaeb116e17fb94426149d23043ce9a1d9695a4
vokoshyv <ronsuij@gmail.com>
2019-01-31 12:18:25 -0800

    (feat) initializes file for student_course_matching.js

--- russianDollEnvelopes.js
+++ russianDollEnvelopes.js
@@ -2,6 +2,51 @@
  * @param {number[][]} envelopes
  * @return {number}
  */
-var maxEnvelopes = function(envelopes) {
 
+class Graph {
+  constructor() {
+    this.storage = new Map();
+  }
+
+  insertNodes(envelope) {
+    this.storage.set(envelope, []);
+  }
+}
+
+
+var maxEnvelopes = function(envelopes) {
+  let graph = new Graph();
+
+  for (let i = 0; i < envelopes.length; i++) {
+    graph.insertNodes(envelopes[i]);
+  }
+
+  console.log(graph);
 };
+
+// [width, height]
+console.log(maxEnvelopes([[5,4],[6,4],[6,7],[2,3]]));
+
+
+
+// => [ [ 2, 3 ], [ 5, 4 ], [ 6, 4 ], [ 6, 7 ] ]
+
+
+// house robber approach <- dead
+// =>      [[2,3],[5,4],[6,15],[7,6],[8,7],[9,8],[10,9]]
+
+// result = [  1 ,  2  ,  3   ,  3  ,   ]
+
+
+// sliding window - not as useful
+//            L              R
+// =>      [[2,3],[5,4],[6,15],[7,6],[8,7],[9,8],[10,9]]
+
+
+// two pointer apraoch from opposite ends of the array
+//
+//                  L                        R
+// =>      [[2,3],[5,4],[6,15],[7,6],[8,7],[9,8],[10,9]]
+
+// count = 2
+// count = 2
