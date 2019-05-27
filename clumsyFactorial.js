ac8cd0b64506281761d1f97247eef18c99c70dd6
vokoshyv <ronsuij@gmail.com>
2019-05-26 22:26:46 -0700

    will return to clumsyFactorial

--- clumsyFactorial.js
+++ clumsyFactorial.js
@@ -3,5 +3,17 @@
  * @return {number}
  */
 var clumsy = function(N) {
+  let result = N;
+  N--;
+  let operation = 'divide';
 
+  while (N > 1) {
+    if (operation === 'divide') {
+      result = Math.floor(result / N);
+      operation = 'addition';
+    } else 
+  }
+
+
+  return result;
 };
