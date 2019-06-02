62fe906b97b2dccc1c9893ee22848c286dfe7bac
vokoshyv <ronsuij@gmail.com>
2019-06-02 12:13:42 -0700

    (feat) will return to recoverTreeFromPreorderTraversal.js

--- recoverTreeFromPreorderTraversal.js
+++ recoverTreeFromPreorderTraversal.js
@@ -10,5 +10,42 @@
  * @return {TreeNode}
  */
 var recoverFromPreorder = function(S) {
+  let modified = [];
+  let chunk = '';
 
+  let i = 0, j = 0;
+
+  while (j < S.length) {
+    chunk = S.slice(i, j);
+    modified.push(formatNode(chunk));
+    i = j;
+
+    while (S[j] !== '-' || j >= S.length) {
+      j++;
+    }
+    console.log(modified);
+  }
+  // chunk = S.slice(i, j);
+  // modified.push(formatNode(chunk));
+
+  console.log(modified);
 };
+
+function formatNode(str) {
+  let depth = 0;
+  let i = 0;
+  while (i < str.length) {
+    if (str[i] === '-') {
+      depth++;
+    } else {
+      break;
+    }
+  }
+
+  let value = parseInt(str.slice(i));
+
+  return [value, depth];
+}
+
+
+recoverFromPreorder("1-2--3--4-5--6--7");
