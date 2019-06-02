3aef4ae199c082a88318be5c5bbfc35551c28026
vokoshyv <ronsuij@gmail.com>
2019-06-02 11:50:20 -0700

    (feat) initializes file for recover tree

---
+++ recoverTreeFromPreorderTraversal.js
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {string} S
+ * @return {TreeNode}
+ */
+var recoverFromPreorder = function(S) {
+
+};
