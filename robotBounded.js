cebe0e60c106bc53afc67d474241628323ab59d5
vokoshyv <ronsuij@gmail.com>
2021-04-16 02:16:13 -0600

    (feat) implements more efficient solution for isRobotBounded.js

--- robotBounded.js
+++ robotBounded.js
@@ -2,6 +2,60 @@
  * @param {string} instructions
  * @return {boolean}
  */
+// var isRobotBounded = function(instructions) {
+//   let directions = {
+//     "N": [0, -1],
+//     "S": [0, 1],
+//     "E": [1, 0],
+//     "W": [-1, 0]
+//   }
+//
+//   let rightTransform = {
+//     "N": "E",
+//     "S": "W",
+//     "E": "S",
+//     "W": "N"
+//   }
+//
+//   let leftTransform = {
+//     "N": "W",
+//     "S": "E",
+//     "E": "N",
+//     "W": "S"
+//   }
+//
+//   let current = "N";
+//
+//   let x = 0;
+//   let y = 0;
+//
+//   function command(char) {
+//     if (char === 'G') {
+//       x += directions[current][0];
+//       y += directions[current][1];
+//     } else if (char === 'R') {
+//       current = rightTransform[current];
+//     } else if (char === 'L') {
+//       current = leftTransform[current];
+//     }
+//   }
+//
+//   for (let i = 0; i < 100; i++) {
+//     for (let j = 0; j < instructions.length; j++) {
+//       command(instructions[j]);
+//     }
+//   }
+//
+//   return x === 0 && y === 0 && current === "N";
+// };
+//
+//
+
+
+/**
+ * @param {string} instructions
+ * @return {boolean}
+ */
 var isRobotBounded = function(instructions) {
   let directions = {
     "N": [0, -1],
@@ -40,12 +94,11 @@ var isRobotBounded = function(instructions) {
     }
   }
 
-  for (let i = 0; i < 100; i++) {
-    for (let j = 0; j < instructions.length; j++) {
-      command(instructions[j]);
-    }
+  for (let j = 0; j < instructions.length; j++) {
+    command(instructions[j]);
   }
 
-  return x === 0 && y === 0 && current === "N";
+  return current !== "N" || (x === 0 && y === 0);
 };
-console.log(isRobotBounded("GL"));
+
+console.log(isRobotBounded("GG"));
