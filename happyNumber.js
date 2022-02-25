f290599685e7d9115bf4ab09498d794346d07725
Ron Tsui <rontsui@Rons-MacBook-Pro.local>
2022-02-24 18:10:03 -0700

    (feat) implements recursive solution for happyNumber.js once again'

--- happyNumber.js
+++ happyNumber.js
@@ -2,13 +2,30 @@
  * @param {number} n
  * @return {boolean}
  */
-var isHappy = function(n) {
-    let visited = new Set();
+const isHappy = (n) => {
+  const visited = new Set();
 
-    const transform = (current) => {
-        if (current === 1) {
-            return true;
-        }
-
+  const transform = (current) => {
+    if (current === 1) {
+      return true;
     }
+    if (visited.has(current)) {
+      return false;
+    }
+
+    visited.add(current);
+
+    const str = current.toString();
+    let next = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      next += (parseInt(str[i]) * parseInt(str[i]));
+    }
+
+    return transform(next);
+  };
+
+  return transform(n);
 };
+
+console.log(isHappy(84));
