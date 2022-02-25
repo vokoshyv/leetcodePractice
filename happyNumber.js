84cb1505b1b1e13b4499e2e818f2113dfa23f867
Ron Tsui <rontsui@Rons-MacBook-Pro.local>
2022-02-24 17:45:44 -0700

    (feat) returning to happyNumber.js

--- happyNumber.js
+++ happyNumber.js
@@ -3,27 +3,12 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-  var temp;
-  var lib = {};
-  var count = 1;
+    let visited = new Set();
 
-  while (n !== 1){
-    // if (lib[n] !== undefined){
-    //   return false;
-    // }
+    const transform = (current) => {
+        if (current === 1) {
+            return true;
+        }
 
-    if (count > 10000){
-      console.log(false);
-      return false;
     }
-    n = (n).toString().split('').reduce(function(prev, curr){
-      return Number(prev) * Number(prev) + Number(curr) * Number(curr);
-    })
-    count++;
-  }
-
-  console.log(true);
-  return true;
 };
-
-isHappy(13);
