614d6d51893ac2c6996ce24a6b26c2cfef99609b
vokoshyv <ronsuij@gmail.com>
2024-01-19 21:41:17 +0000

    (feat) reimplements solution to minimumFallingPathSum.js

--- minimumFallingPathSum.js
+++ minimumFallingPathSum.js
@@ -1,30 +1,40 @@
 /**
- * @param {number[][]} A
+ * @param {number[][]} matrix
  * @return {number}
  */
-var minFallingPathSum = function(A) {
-  let y = 1;
 
-  while (y < A.length) {
-    for (let x = 0; x < A.length; x++) {
-      A[y][x] += findMinAbove(A, x, y);
+const chooseSmallest = (matrix, x, y) => {
+    let option1 = Infinity;
+    let option2 = Infinity;
+    let option3 = Infinity;
+
+    if (x > 0) {
+        option1 = matrix[y - 1][x - 1];
+    }
+    option2 = matrix[y - 1][x];
+    if (x < matrix[0].length - 1) {
+        option3 = matrix[y - 1][x + 1];
     }
 
-    if (y === A.length - 1) {
-      return Math.min(...A[y]);
-    }
-    y++;
-  }
-
-  return A[0][0];
-};
-
-function findMinAbove(A, x, y) {
-  if (x === 0) {
-    return Math.min(A[y-1][x], A[y-1][x+1]);
-  } else if (x === (A.length - 1)) {
-    return Math.min(A[y-1][x], A[y-1][x-1]);
-  } else {
-    return Math.min(A[y-1][x], A[y-1][x+1], A[y-1][x-1]);
-  }
+    return Math.min(option1, option2, option3);
 }
+
+
+var minFallingPathSum = function(matrix) {
+    let matrixHeight = matrix.length;
+    let matrixWidth = matrix[0].length;
+
+    for (let y = 1; y < matrixHeight; y++) {
+        for (let x = 0; x < matrixWidth; x++) {
+            matrix[y][x] = matrix[y][x] + chooseSmallest(matrix, x, y);
+        }
+    }
+
+    let result = Infinity;
+
+    for (let x = 0; x < matrixWidth; x++) {
+        result = Math.min(result, matrix[matrixHeight - 1][x]);
+    }
+
+    return result;
+};
