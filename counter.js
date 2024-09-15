ecf7b499b988a515670ef7ac4cfcc96be2d20fc1
vokoshyv <ronsuij@gmail.com>
2024-09-15 18:40:40 +0100

    (feat) initializes solution for counter.js

--- counter.js
+++ counter.js
@@ -1,17 +1,1 @@
-/**
- * @param {number} n
- * @return {Function} counter
- */
-var createCounter = function(n) {
-    
-    return function() {
-        
-    };
-};
-
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+var createCounter=function(n){return function(){return n++}}
