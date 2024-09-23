541a7eb57fc614d11140d3b76650f4432bb76510
vokoshyv <ronsuij@gmail.com>
2024-09-23 20:22:52 +0100

    (feat) initializes file for walkingRobotSimulation2.js

---
+++ walkingRobotSimulation2.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} width
+ * @param {number} height
+ */
+var Robot = function(width, height) {
+    
+};
+
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+Robot.prototype.step = function(num) {
+    
+};
+
+/**
+ * @return {number[]}
+ */
+Robot.prototype.getPos = function() {
+    
+};
+
+/**
+ * @return {string}
+ */
+Robot.prototype.getDir = function() {
+    
+};
+
+/** 
+ * Your Robot object will be instantiated and called as such:
+ * var obj = new Robot(width, height)
+ * obj.step(num)
+ * var param_2 = obj.getPos()
+ * var param_3 = obj.getDir()
+ */
