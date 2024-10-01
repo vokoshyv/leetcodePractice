bd4d25522e9e1be6d3087f3e55af504d18e58d58
vokoshyv <ronsuij@gmail.com>
2024-10-01 16:15:53 +0100

    (feat) implements solution for walkingRobotSimulation2.js; albeit inefficient as we're traveling a single step

--- walkingRobotSimulation2.js
+++ walkingRobotSimulation2.js
@@ -3,29 +3,81 @@
  * @param {number} height
  */
 var Robot = function(width, height) {
-    
+    this.width = width;
+    this.height = height;
+    this.heightPlusWidth = width + height;
+    this.x = 0;
+    this.y = 0;    
+    this.direction = "East";
+    this.xDelta = 1;
+    this.yDelta = 0;
 };
+
+Robot.prototype.changeDirection = function() {
+    if (this.direction === "East") {
+        this.direction = "North";
+        this.xDelta = 0;
+        this.yDelta = 1;
+    } else if (this.direction === "North") {
+        this.direction = "West";
+        this.xDelta = -1;
+        this.yDelta = 0;
+    } else if (this.direction === "West") {
+        this.direction = "South";
+        this.xDelta = 0;
+        this.yDelta = -1;
+    } else if (this.direction === "South") {
+        this.direction = "East";
+        this.xDelta = 1;
+        this.yDelta = 0;
+    }
+};
+
+Robot.prototype.proceedOneStep = function() {
+    if (this.direction === "East" && this.x === (this.width - 1)) {
+        this.changeDirection();
+    } else if (this.direction === "North" && this.y === (this.height - 1)) {
+        this.changeDirection();
+    } else if (this.direction === "West" && this.x === 0) {
+        this.changeDirection();
+    } else if (this.direction === "South" && this.y === 0) {
+        this.changeDirection();
+    }
+
+    this.x += this.xDelta;
+    this.y += this.yDelta;
+}
 
 /** 
  * @param {number} num
  * @return {void}
  */
 Robot.prototype.step = function(num) {
-    
+    if (this.x === 0 && this.y === 0 && num > 0) {
+        this.proceedOneStep();
+        num--;
+    }
+
+    num = num % (2 * this.heightPlusWidth - 4);
+
+    while (num > 0) {
+        this.proceedOneStep();
+        num--;
+    }
 };
 
 /**
  * @return {number[]}
  */
 Robot.prototype.getPos = function() {
-    
+    return [this.x, this.y];
 };
 
 /**
  * @return {string}
  */
 Robot.prototype.getDir = function() {
-    
+    return this.direction;    
 };
 
 /** 
