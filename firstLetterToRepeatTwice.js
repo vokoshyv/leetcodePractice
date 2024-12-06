f2da5326321eb8cd8a058a9434a2aa314dfc9ba1
vokoshyv <ronsuij@gmail.com>
2024-12-06 22:46:00 +0000

    (feat) attempts removing spaces to test out memory usage; unchanged

--- firstLetterToRepeatTwice.js
+++ firstLetterToRepeatTwice.js
@@ -2,15 +2,4 @@
  * @param {string} s
  * @return {character}
  */
-var repeatedCharacter = function(s) {
-    let lib = new Set();
-    let sLength = s.length;
-
-    for (let i = 0; i < sLength; i++) {
-        let char = s[i];
-        if (lib.has(char)) {
-            return char;
-        }
-        lib.add(char);
-    }
-};
+var repeatedCharacter = function(s) { let lib = new Set();let sLength = s.length;for (let i = 0; i < sLength; i++) {let char = s[i];if (lib.has(char)) {return char;}lib.add(char);}};
