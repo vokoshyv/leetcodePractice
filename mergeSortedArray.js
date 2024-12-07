4edd5a038cc40623a0338f245e8ed69f5d14f511
vokoshyv <ronsuij@gmail.com>
2024-12-07 16:27:51 +0000

    (feat) implements proper solution to mergeSortedArray.js

--- mergeSortedArray.js
+++ mergeSortedArray.js
@@ -5,32 +5,70 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-    var endPointer = nums1.length - 1;
+var merge = function (nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = 0;
 
-    var i = 0;
-    var j = 0;
+    let shiftI = nums1.length - 1;
 
-    while (i < m && j < n){
-      if (nums2[j] < nums1[i]){
-        var temp = nums2[j];
-        nums2[j] = nums1[i];
-        nums1[i] = temp;
-        j++;
-      } else {
+    while (shiftI >= n) {
+        nums1[shiftI] = nums1[i];
+        i--;
+        shiftI--;
+    }
+
+    console.log("NUMS1: ", nums1);
+    
+    i = n;
+    let k = 0;
+    
+    while (m !== 0 && i < nums1.length && j < n) {
+        if (nums1[i] < nums2[j]) {
+            nums1[k] = nums1[i];
+            i++;
+        } else {
+            nums1[k] = nums2[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < nums1.length) {
+        nums1[k] = nums1[i];
         i++;
-      }
+        k++;
     }
 
-    console.log('i: ', i);
-    console.log('j: ', j);
-
-    while (j < n){
-      nums1[i] = nums2[j];
-      i++;
-      j++;
+    while (j < nums2.length) {
+        nums1[k] = nums2[j];
+        j++;
+        k++;
     }
-
+    
+    // console.log("NUMS1: ", nums1);
+    // console.log("NUMS2: ", nums2);
 };
 
-console.log(merge([4,5,6,0,0,0], 3, [1,2,3], 3));
+console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
+
+console.log(merge([2,3,6,0,0,0], 3, [1,3,7], 3));
+
+
+/*
+
+
+ i
+[1,2,3,0,0,0]
+
+ j
+[2,5,6]
+
+
+ i
+[2,3,6,0,0,0]
+
+ j
+[1,3,7]
+
+
+*/
