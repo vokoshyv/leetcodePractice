51d914ce5dee3609a155914cc9b5783f81fcb9bc
vokoshyv <ronsuij@gmail.com>
2024-12-09 18:39:26 +0000

    (feat) returns to isPalindrome and creates cleaner solution

--- validPalindrome.js
+++ validPalindrome.js
@@ -2,24 +2,36 @@
  * @param {string} s
  * @return {boolean}
  */
+let lib = new Set();
+let toAdd = 'qwertyuiopasdfghjklzxcvbnm1234567890';
+toAdd.split('').forEach((char) => {lib.add(char)});
+
 var isPalindrome = function(s) {
-  var lib = '1234567890abcdefghijklmnopqrstuvwxyz'
-  var libObject = {};
-  for (var i = 0; i < lib.length; i++){
-    libObject[lib[i]] = true;
-  }
+    s = s.toLowerCase();
 
-  var work = '';
-  for (var i = 0; i < s.length; i++){
-    if (libObject[s[i].toLowerCase()]){
-      work += s[i].toLowerCase();
+    let i = 0; 
+    let j = s.length - 1;
+
+    while (i < j) {
+        while (i < s.length && !lib.has(s[i])) {
+            i++;
+        }
+        while (j > -1 && !lib.has(s[j])) {
+            j--;
+        }
+
+        if (i > j) {
+            break;
+        }
+
+
+        if (s[i] !== s[j]) {
+            return false;
+        }
+
+        i++;
+        j--;
     }
-  }
 
-  var index = work.length;
-  var workCompare = '';
-  while (index--){
-    workCompare += work[index];
-  }
-  return work === workCompare;
+    return true;
 };
