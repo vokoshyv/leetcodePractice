d226aacf27eeeb1f82ae8a233b4f76711342f8dc
vokoshyv <ronsuij@gmail.com>
2024-12-14 17:59:51 +0000

    (feat) reimplements solution for validParentheses.js

--- validParentheses.js
+++ validParentheses.js
@@ -3,5 +3,26 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    const stack = [];
+    const match = {
+        ')': '(', 
+        '}': '{', 
+        ']': '[' 
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+
+        if (char in match) {
+            let popped = stack.pop();
+
+            if (match[char] !== popped) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
 };
