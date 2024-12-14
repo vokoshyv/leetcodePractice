c4fe3970d2f7a9ca2f24e3e1b1233973721ed6df
vokoshyv <ronsuij@gmail.com>
2024-12-14 18:07:24 +0000

    (feat) implements cleaner solution to minstack.jsgst

--- minstack.js
+++ minstack.js
@@ -1,52 +1,52 @@
-/**
- * initialize your data structure here.
+var MinStack = function () {
+    this.stack1 = [];
+    this.stack2 = [];
+};
+
+const getLastOfArray = (arr) => {
+    return arr[arr.length - 1];
+}
+
+/** 
+ * @param {number} val
+ * @return {void}
  */
-var MinStack = function() {
-  this.stack1 = [];
-  this.stack2 = [];
+MinStack.prototype.push = function (val) {
+    this.stack1.push(val);
+
+    if (this.stack2.length > 0) {
+        this.stack2.push(Math.min(getLastOfArray(this.stack2), val));
+    } else {
+        this.stack2.push(val);
+    }
 };
 
 /**
- * @param {number} x
  * @return {void}
  */
-MinStack.prototype.push = function(x) {
-  this.stack1.push(x);
-  if (this.stack1.length === 1) {
-    this.stack2.push(x);
-  } else if (x < this.stack2[this.stack2.length - 1]) {
-    this.stack2.push(x);
-  } else {
-    this.stack2.push(this.stack2[this.stack2.length - 1]);
-  }
-};
-
-/**
- * @return {void}
- */
-MinStack.prototype.pop = function() {
-  this.stack1.pop();
-  this.stack2.pop();
+MinStack.prototype.pop = function () {
+    this.stack2.pop();
+    return this.stack1.pop();
 };
 
 /**
  * @return {number}
  */
-MinStack.prototype.top = function() {
-  return this.stack1[this.stack1.length - 1];
+MinStack.prototype.top = function () {
+    return getLastOfArray(this.stack1);
 };
 
 /**
  * @return {number}
  */
-MinStack.prototype.getMin = function() {
-  return this.stack2[this.stack2.length - 1];
+MinStack.prototype.getMin = function () {
+    return getLastOfArray(this.stack2);
 };
 
-/**
+/** 
  * Your MinStack object will be instantiated and called as such:
  * var obj = new MinStack()
- * obj.push(x)
+ * obj.push(val)
  * obj.pop()
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
