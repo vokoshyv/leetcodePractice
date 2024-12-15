6c5d4a8caa4310611f62322847486802c3ef82e8
vokoshyv <ronsuij@gmail.com>
2024-12-15 11:53:56 +0000

    (feat) implements recursive solution for isSubsequence.js

--- isSubsequence.js
+++ isSubsequence.js
@@ -3,6 +3,19 @@
  * @param {string} t
  * @return {boolean}
  */
-var isSubsequence = function(s, t) {
-    
+var isSubsequence = function (s, t) {
+    const check = (i, j) => {
+        if (i >= s.length) {
+            return true;
+        }
+        if (j >= t.length) {
+            return false;
+        }
+        if (s[i] === t[j]) {
+            return check(i + 1, j + 1);
+        }
+        return check(i, j + 1);
+    }
+
+    return check(0, 0);
 };
